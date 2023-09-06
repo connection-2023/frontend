2f215c7f3aebbb65230d99549f7340d4ce1115c8
hyeonjun-L <beeminor60@gmail.com>
2023-09-07 04:25:19 +0900

    Feat: 무한 캐러셀 구현 및 최적화 모드 구현

--- src/components/CardImg/CardImg.tsx
+++ src/components/CardImg/CardImg.tsx
@@ -4,12 +4,29 @@ import Image from 'next/image';
 interface CardImgProps {
   imgURL: string[];
   focus: boolean;
+  arrow?: boolean;
+  optimizationMode?: boolean;
 }
 
-const CardImg = ({ imgURL, focus }: CardImgProps) => {
-  const [currentImgIndex, setCurrentImgIndex] = useState(0);
-  const [loadedImagesCount, setLoadedImagesCount] = useState(1);
+const CardImg = ({
+  imgURL: originalImgURL,
+  focus,
+  arrow = true,
+  optimizationMode = true,
+}: CardImgProps) => {
+  const imgURL = [
+    originalImgURL.at(-1),
+    ...originalImgURL,
+    ...originalImgURL.slice(0, 2),
+  ];
+
   const imgLength = imgURL.length;
+
+  const [currentImgIndex, setCurrentImgIndex] = useState(0);
+  const [loadedImagesCount, setLoadedImagesCount] = useState(
+    optimizationMode ? 1 : imgLength,
+  );
+  const [isAnimating, setIsAnimating] = useState(true);
 
   useEffect(() => {
     if (focus && loadedImagesCount < imgLength) {
@@ -22,7 +39,17 @@ const CardImg = ({ imgURL, focus }: CardImgProps) => {
 
     if (focus) {
       timer = setInterval(() => {
-        setCurrentImgIndex((prev) => (prev + 1 === imgLength ? 0 : prev + 1));
+        setCurrentImgIndex((prev) => {
+          if (prev + 1 === imgLength - 2) {
+            setIsAnimating(false);
+            return 0;
+          } else if (prev === 0) {
+            setIsAnimating(true);
+            return prev + 1;
+          } else {
+            return prev + 1;
+          }
+        });
       }, 1300);
     } else {
       setCurrentImgIndex(0);
@@ -35,36 +62,54 @@ const CardImg = ({ imgURL, focus }: CardImgProps) => {
   }, [focus, loadedImagesCount]);
 
   return (
-    <div className="display: relative flex h-full">
+    <div className="display: flex h-full ">
       {imgURL.slice(0, loadedImagesCount).map((imgSrc, index) => (
         <picture
-          key={imgSrc}
+          key={index}
           className="relative h-full w-full flex-shrink-0"
           style={{
             transform: `translateX(-${100 * currentImgIndex}%)`,
-            transition: 'transform ease-out .5s',
+            transition: isAnimating ? 'transform ease-out 1.1s' : 'none',
           }}
         >
-          <Image
-            src={imgSrc}
-            alt="Connection 댄스 이미지"
-            fill
-            sizes="(max-width: 720px) 40vw, (max-width: 1440px) 20vw"
-            priority={index === 0}
-          />
+          {imgSrc && (
+            <Image
+              src={imgSrc}
+              alt="Connection 댄스 이미지"
+              fill
+              sizes="(max-width: 720px) 60vw, (max-width: 1440px) 30vw"
+              priority={index === 0 || !optimizationMode}
+            />
+          )}
         </picture>
       ))}
+
       {focus && (
         <div className="display: absolute bottom-0 flex h-[10%] w-full items-center justify-center bg-black/[.5]">
-          {imgURL.map((_, index) => (
+          {originalImgURL.map((_, index) => (
             <span
               key={index}
               className={`mr-1.5 inline-block h-1.5 w-1.5 rounded-full ${
-                index < currentImgIndex + 1 ? 'bg-white' : 'bg-neutral-500'
+                index === 0 ||
+                (currentImgIndex < originalImgURL.length &&
+                  index <= currentImgIndex)
+                  ? 'bg-white'
+                  : 'bg-neutral-500'
               }`}
             />
           ))}
         </div>
+      )}
+
+      {focus && arrow && (
+        <>
+          <div className="absolute left-3 top-1/2 -translate-y-1/2 transform">
+            뒤로
+          </div>
+          <div className="absolute right-3 top-1/2 -translate-y-1/2 transform">
+            앞으로
+          </div>
+        </>
       )}
     </div>
   );
