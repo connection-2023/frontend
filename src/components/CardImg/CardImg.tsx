5e121795f342873574f3c733129af3d6051322fa
hyeonjun-L <beeminor60@gmail.com>
2023-09-09 02:23:28 +0900

    Refactor: 코드 리팩터링 및 컴포넌트 설명 입력

--- src/components/CardImg/CardImg.tsx
+++ src/components/CardImg/CardImg.tsx
@@ -2,6 +2,29 @@ import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { Arrow } from '../../../public/icons/svg';
 
+/**
+ * CardImgProps Interface
+ *
+ * 사용법 1
+ * <relative overflow-hidden 컨테이너>
+ *  <이미지 크기 제어 컨테이너>
+ *    <CardImg/>
+ *  </이미지 크기 제어 컨테이너>
+ * </ relative overflow-hidden 컨테이너>
+ *
+ * 사용법 2
+ * <이미지 크기 제어 및 relative overflow-hidden>
+ *   <CardImg/>
+ * </ 이미지 크기 제어 및 relative overflow-hidden>
+ *
+ * @property {string[]} imgURL - 표시할 이미지들의 URL들이 담긴 배열
+ * @property {boolean} move - 이미지 캐러셀 활성화
+ * @property {boolean} [arrow=true] - 탐색을 위해 화살표를 표시해야 하는지 여부를 나타내는 선택적 플래그 (기본값 = true)
+ * @property {boolean} [optimizationMode=true] - true => 이미지 한개만 미리 로드 move true 변경시 나머지 이미지 로드, false => 이미지 배열 전부를 로드 (기본값 = true)
+ * @property {boolean} [showCurrentImage=true] - 현재 캐러셀 이미지 표시의 상태창 표시 여부를 나타내는 선택적 플래그 (기본값 = true)
+ * @property {number} [gap=0] - 이미지 사이의 간격을 rem으로 지정하는 선택적 숫자 (기본값 = 0)
+ */
+
 interface CardImgProps {
   imgURL: string[];
   move: boolean;
@@ -11,7 +34,12 @@ interface CardImgProps {
   gap?: number;
 }
 
-let timer: NodeJS.Timeout | null = null;
+const IMG_MOVE_INTERVAL_TIME = 2000;
+const ARROW_WAIT_TIME = 2000;
+
+let intervalId: NodeJS.Timeout | null = null;
+let timeoutId: NodeJS.Timeout | null = null;
+
 const CardImg = ({
   imgURL: originalImgURL,
   move,
@@ -25,75 +53,88 @@ const CardImg = ({
     ...originalImgURL,
     ...originalImgURL.slice(0, 2),
   ];
-
   const imgLength = imgURL.length;
+
   const [currentImgIndex, setCurrentImgIndex] = useState(0);
   const [loadedImagesCount, setLoadedImagesCount] = useState(
     optimizationMode ? 1 : imgLength,
   );
   const [isAnimating, setIsAnimating] = useState(true);
 
-  useEffect(() => {
-    if (move && loadedImagesCount < imgLength) {
-      setLoadedImagesCount(imgLength);
-    }
-  }, [move]);
+  if (move && loadedImagesCount < imgLength) {
+    setLoadedImagesCount(imgLength);
+  }
+
+  const updateImageIndex = () => {
+    setCurrentImgIndex((prev) => {
+      if (prev + 1 === imgLength - 2) {
+        setIsAnimating(false);
+        return 0;
+      } else if (prev === 0) {
+        setIsAnimating(true);
+        return prev + 1;
+      } else {
+        return prev + 1;
+      }
+    });
+  };
 
   useEffect(() => {
-    if (move) {
-      timer = setInterval(() => {
-        setCurrentImgIndex((prev) => {
-          if (prev + 1 === imgLength - 2) {
-            setIsAnimating(false);
-            return 0;
-          } else if (prev === 0) {
-            setIsAnimating(true);
-            return prev + 1;
-          } else {
-            return prev + 1;
-          }
-        });
-      }, 1300);
+    if (move && isAnimating) {
+      if (optimizationMode) {
+        updateImageIndex();
+      }
+      intervalId = setInterval(updateImageIndex, IMG_MOVE_INTERVAL_TIME);
     } else {
+      setIsAnimating(false);
       setCurrentImgIndex(0);
-      if (timer) clearInterval(timer);
+      setTimeout(() => setIsAnimating(true), 100);
+      if (intervalId) clearInterval(intervalId);
     }
 
     return () => {
-      if (timer) clearInterval(timer);
+      if (intervalId) clearInterval(intervalId);
     };
-  }, [move, loadedImagesCount, isAnimating]);
+  }, [move, loadedImagesCount]);
 
-  const goBackward = () => {
+  const changeImage = (direction: 'BACKWARD' | 'FORWARD') => {
+    if (intervalId) clearInterval(intervalId);
+    if (timeoutId) clearTimeout(timeoutId);
+
     setIsAnimating(false);
     setCurrentImgIndex((prev) =>
-      prev === 0 ? originalImgURL.length - 1 : prev - 1,
+      direction === 'BACKWARD'
+        ? prev === 0
+          ? originalImgURL.length - 1
+          : prev - 1
+        : prev === originalImgURL.length - 1
+        ? 0
+        : prev + 1,
     );
-    if (timer) clearInterval(timer);
-    setTimeout(() => setIsAnimating(true), 1300);
-  };
 
-  const goForward = () => {
-    setIsAnimating(false);
-    setCurrentImgIndex((prev) =>
-      prev === originalImgURL.length - 1 ? 0 : prev + 1,
-    );
-    if (timer) clearInterval(timer);
-    setTimeout(() => setIsAnimating(true), 1300);
+    timeoutId = setTimeout(() => {
+      setIsAnimating(true);
+      intervalId = setInterval(updateImageIndex, IMG_MOVE_INTERVAL_TIME);
+    }, ARROW_WAIT_TIME);
   };
 
   return (
-    <div className="display: flex h-full">
+    <div
+      className="display: flex h-full"
+      style={{
+        gap: `${gap}rem`,
+      }}
+    >
       {imgURL.slice(0, loadedImagesCount).map((imgSrc, index) => (
         <picture
           key={index}
-          className="relative h-full w-full flex-shrink-0"
+          className={`relative h-full w-full flex-shrink-0 ${
+            isAnimating && 'transition-transform duration-[1600ms] ease-out'
+          }`}
           style={{
             transform: `translateX(calc(-${100 * currentImgIndex}% - ${
               gap * currentImgIndex
             }rem)`,
-            transition: isAnimating ? 'transform ease-out 1.1s' : 'none',
-            marginRight: `${gap}rem`,
           }}
         >
           {imgSrc && (
@@ -108,7 +149,7 @@ const CardImg = ({
         </picture>
       ))}
 
-      {move && showCurrentImage && (
+      {showCurrentImage && (
         <div className="display: absolute bottom-0 flex h-[10%] w-full items-center justify-center bg-black/[.5]">
           {originalImgURL.map((_, index) => (
             <span
@@ -125,20 +166,16 @@ const CardImg = ({
         </div>
       )}
 
-      {move && arrow && (
+      {arrow && (
         <>
           <Arrow
-            onClick={goBackward}
+            onClick={() => changeImage('BACKWARD')}
             className="absolute left-3 top-1/2 -translate-y-1/2 -scale-x-100 transform cursor-pointer"
-          >
-            뒤로
-          </Arrow>
+          />
           <Arrow
-            onClick={goForward}
+            onClick={() => changeImage('FORWARD')}
             className="absolute right-3 top-1/2 -translate-y-1/2 transform cursor-pointer"
-          >
-            앞으로
-          </Arrow>
+          />
         </>
       )}
     </div>
