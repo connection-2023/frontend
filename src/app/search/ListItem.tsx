cf82c3bc7b6b9b29ceb93fa5d822221af1a2ceb1
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-09-11 21:01:25 +0900

    feat(#25): 검색 페이지 최근 검색어 구현

---
+++ src/app/search/ListItem.tsx
@@ -0,0 +1,9 @@
+const ListItem = ({ label }: { label: string }) => {
+  return (
+    <li className="mb-1 h-[38px] cursor-pointer whitespace-nowrap rounded-[3.125rem] bg-[#F5F5F5] px-[1.06rem] py-[0.44rem] text-base text-black">
+      {label}
+    </li>
+  );
+};
+
+export default ListItem;
---
+++ src/app/search/MyKeyword.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { useState } from 'react';
+import ListItem from './ListItem';
+
+const MyKeyword = () => {
+  const [userKeywords, setUserKeywords] = useState([
+    'K-pop',
+    '원데이 클래스',
+    '스트릿 댄스',
+  ]);
+  const onClickRemoveAll = () => {
+    setUserKeywords([]);
+  };
+
+  return (
+    <>
+      <div className="mb-[0.88rem] flex items-center justify-between">
+        <h1 className="text-[rgba(0, 0, 0, 0.90)] text-lg font-bold">
+          최근 검색어
+        </h1>
+        <button
+          className="text-sm font-bold text-[#B6B6B6]"
+          onClick={onClickRemoveAll}
+        >
+          전체삭제
+        </button>
+      </div>
+      <ul className="flex h-[58px] gap-2 overflow-x-auto">
+        {userKeywords.map((keyword, i) => (
+          <ListItem label={keyword} key={i} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default MyKeyword;
