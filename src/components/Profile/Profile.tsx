bc182f3e28a85c47ce3a24fe5509ddd430cd79e5
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-09-11 23:20:41 +0900

    feat: 프로필 공통 컴포넌트 size props 추가

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,21 +4,26 @@ import { ProfileSVG } from '../../../public/icons/svg';
 interface ProfileProps {
   src?: string;
   nickname: string;
+  size: number;
 }
 
 const commonProps = {
-  width: 34,
-  height: 34,
-  className: 'mr-1.5 h-[2.125rem] w-[2.125rem] rounded-[2.125rem]',
+  className: 'mr-1.5 rounded-[2.125rem]',
 };
 
-const Profile = ({ src, nickname }: ProfileProps) => {
+const Profile = ({ src, nickname, size }: ProfileProps) => {
   return (
-    <div className="color-inherit flex items-center text-sm">
+    <div className="color-inherit flex items-center">
       {src ? (
-        <Image src={src} alt="프로필 사진" {...commonProps} />
+        <Image
+          src={src}
+          width={size}
+          height={size}
+          alt="프로필 사진"
+          {...commonProps}
+        />
       ) : (
-        <ProfileSVG {...commonProps} />
+        <ProfileSVG width={size} height={size} {...commonProps} />
       )}
       <span>{nickname}</span>
     </div>
