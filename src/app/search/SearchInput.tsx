ef95d95cea7525f919886817cf271eb6104df015
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-09-11 20:59:59 +0900

    feat(#25): 검색 페이지 검색바 구현

---
+++ src/app/search/SearchInput.tsx
@@ -0,0 +1,65 @@
+'use client';
+import { useState } from 'react';
+import { SearchSVG, ClearSVG } from '../../../public/icons/svg';
+
+const SearchInput = () => {
+  const [keyword, setKeyword] = useState('');
+
+  const suggestions = [
+    '댄스',
+    '댄스 영상',
+    '댄서',
+    '댄스팀',
+    '레슨',
+    '방송댄스',
+    '폴댄스',
+    '댄싱',
+  ];
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
+  };
+
+  const onClickClear = () => {
+    setKeyword('');
+  };
+
+  return (
+    <div className="border-box flex h-full w-full items-center">
+      <SearchSVG width={22} height={22} className="h-[22px] w-[22px]" />
+      <input
+        className="border-box relative mx-2 flex-1 text-lg font-bold focus:outline-none"
+        value={keyword}
+        onChange={handleInputChange}
+        placeholder="검색어를 입력하세요"
+      />
+      <ClearSVG
+        width={21}
+        height={21}
+        onClick={onClickClear}
+        className="mr-1 w-[21px] cursor-pointer"
+      />
+      {keyword.trim() !== '' && (
+        <div className="absolute top-[10rem] flex min-h-[450px] w-[95%] flex-col gap-3 bg-white p-2 text-lg">
+          {suggestions.map((suggestion) => (
+            <p key={suggestion}>
+              {suggestion
+                .split(new RegExp(`(${keyword})`, 'gi'))
+                .map((char, index) =>
+                  char.toLowerCase() === keyword.toLowerCase() ? (
+                    <span key={index} className="text-sub-color1">
+                      {char}
+                    </span>
+                  ) : (
+                    char
+                  ),
+                )}
+            </p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchInput;
