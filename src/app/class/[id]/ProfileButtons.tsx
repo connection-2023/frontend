1e33a3215f7f75490e730bccf61f0a9edd97ce71
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-09-14 00:36:56 +0900

    feat(#26): class 상세 페이지 프로필 버튼 구현

---
+++ src/app/class/[id]/ProfileButtons.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { Button } from '@/components/Button/Button';
+import { ChatSVG } from '../../../../public/icons/svg';
+
+const ProfileButtons = () => {
+  return (
+    <>
+      <Button mode="default" size="medium" onClick={() => {}}>
+        강사 프로필
+      </Button>
+      <Button mode="default" size="medium" onClick={() => {}}>
+        <ChatSVG className="mr-[3px]" />
+        문의하기
+      </Button>
+    </>
+  );
+};
+
+export default ProfileButtons;
