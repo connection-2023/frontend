1584220601470fa9425f86dfe05f30569e73d462
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-09-18 03:14:36 +0900

    feat: class sections 추가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,8 @@
 export const WEEK_DAYS = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+
+export const CLASS_SECTIONS = [
+  { id: 'intro-section', label: '클래스 소개' },
+  { id: 'date-section', label: '일정 및 시간' },
+  { id: 'location-section', label: '진행 장소' },
+  { id: 'review-section', label: '후기' },
+];
