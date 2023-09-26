27a5febd8aad340c26a2a8986e8ddeeb87a6c15a
hyeonjun-L <beeminor60@gmail.com>
2023-09-27 05:22:32 +0900

    refactor: Quill ->  SunEditor로 변경

--- src/app/class/create/_components/ClassExplanation.tsx
+++ src/app/class/create/_components/ClassExplanation.tsx
@@ -1,67 +1,16 @@
-import { useMemo, useRef } from 'react';
-import ReactQuill from 'react-quill';
 import {
   ANNOUNCEMENT,
   CLASS_OPERATION_PLAN,
   QUILL_DEFAULT_VALUE,
-  QUILL_MODULES_CONTAINER,
+  TOOLBAR,
 } from '@/constants/constants';
-import 'react-quill/dist/quill.snow.css';
-import '@/styles/quill.css';
+import SunEditor from 'suneditor-react';
+import 'suneditor/dist/css/suneditor.min.css';
 
 const scrollStyle =
   'scrollbar scrollbar-track-[#F5F5F5] scrollbar-thumb-sub-color2 scrollbar-thumb-rounded-lg scrollbar-w-2';
 
 const ClassExplanation = () => {
-  const quillRef = useRef<ReactQuill | null>(null);
-
-  const imageHandler = () => {
-    const input = document.createElement('input');
-    input.setAttribute('type', 'file');
-    input.setAttribute('accept', 'image/*');
-    input.click();
-
-    input.addEventListener('change', async () => {
-      const file = input.files?.[0];
-
-      if (file && quillRef.current) {
-        const imageURL = await readFileAsDataURL(file);
-        // 추후 S3 업로드 후 변경
-
-        const editor = quillRef.current.getEditor();
-        const range = editor.getSelection();
-        if (range) {
-          editor.insertEmbed(range.index, 'image', imageURL);
-        }
-      }
-    });
-  };
-
-  const readFileAsDataURL = (file: File) => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-
-      reader.onloadend = () => {
-        resolve(reader.result);
-      };
-
-      reader.onerror = reject;
-
-      reader.readAsDataURL(file);
-    });
-  }; //추후 S3로 변경시 삭제 예정
-
-  const modules = useMemo(() => {
-    return {
-      toolbar: {
-        container: QUILL_MODULES_CONTAINER,
-        handlers: {
-          image: imageHandler,
-        },
-      },
-    };
-  }, []);
-
   return (
     <>
       <section className="mt-9 flex flex-col">
@@ -84,11 +33,11 @@ const ClassExplanation = () => {
 
       <section className="mt-9 flex flex-col">
         <h2 className="text-lg font-bold">커리큘럼</h2>
-        <ReactQuill
-          className={`mb-10 mt-3 h-[40rem] ${scrollStyle}`}
-          modules={modules}
+        <SunEditor
+          lang="ko"
+          height="652px"
           defaultValue={QUILL_DEFAULT_VALUE}
-          ref={quillRef}
+          setOptions={{ buttonList: TOOLBAR }}
         />
       </section>
     </>
