410e1821e4ba5cb96707cabfff4a72c4960060bf
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-09-28 04:05:41 +0900

    feat(#61): 메인 인기 강사 컴포넌트 구현

---
+++ src/components/Preview/InstructorPreview.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+interface IPreviewProps {
+  image: string;
+  nickname: string;
+}
+
+const InstructorPreview = ({ image, nickname }: IPreviewProps) => {
+  const src =
+    image ||
+    'https://img.freepik.com/free-photo/pretty-woman-practising-hip-hop-dance_107420-85008.jpg?size=626&ext=jpg';
+  return (
+    <div className="relative flex flex-col">
+      <Image width={148} height={114} src={src} alt="TOP 강사 이미지" />
+      <p className="absolute bottom-0 flex h-[38px] w-full items-center justify-center bg-black text-sm font-bold text-white">
+        {nickname}
+      </p>
+    </div>
+  );
+};
+export default InstructorPreview;
