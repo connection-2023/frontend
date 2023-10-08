5e6acbde037c64d1b19f56c6415c7b7a3ba3c594
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-10-08 22:53:03 +0900

    feat(#81): onClosed props 제거 & next/link 사용

--- src/components/Header.tsx/Auth/Welcome.tsx
+++ src/components/Header.tsx/Auth/Welcome.tsx
@@ -1,11 +1,11 @@
 import { AuthWelcomeSVG } from '@/../public/icons/svg';
+import Link from 'next/link';
 
 interface IWelcome {
   nickname: string;
-  onClosed: () => void;
 }
 
-const Welcome = ({ nickname, onClosed }: IWelcome) => {
+const Welcome = ({ nickname }: IWelcome) => {
   return (
     <section className="flex w-full flex-col items-center justify-center px-[1.31rem] py-4 font-semibold">
       <AuthWelcomeSVG className="mt-4" />
@@ -13,12 +13,12 @@ const Welcome = ({ nickname, onClosed }: IWelcome) => {
       <h2 className="mt-[0.88rem] text-2xl  text-sub-color1">회원가입 완료</h2>
       <h3 className="mt-4 text-base">{nickname}님, 환영합니다!</h3>
 
-      <button
-        onClick={onClosed}
-        className="mt-8 h-[2.8rem] w-full rounded-[0.31rem] bg-sub-color1 text-lg text-white"
+      <Link
+        href="/class"
+        className="mt-8 flex h-[2.8rem] w-full items-center justify-center rounded-[0.31rem] bg-sub-color1 text-lg text-white"
       >
-        시작하기
-      </button>
+        클래스 보러가기
+      </Link>
     </section>
   );
 };
