f453c815ed0897ec54299b3ab68e8d1461ba3350
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-10-09 19:38:03 +0900

    feat(#81): 구글 로그인 구현

---
+++ src/components/Header.tsx/Auth/AuthHome.tsx
@@ -0,0 +1,92 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { getAuth } from '@/app/api/auth/route';
+import KakaoAuth from './KakaoAuth';
+import GoogleAuth from './GoogleAuth';
+import { ConnectionLogoSVG } from '@/../public/icons/svg';
+import { LoginResponse, AuthResponse, SignInResponse } from '@/types/auth';
+
+const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+
+interface IAuthHome {
+  handleStatusCode: (code: number) => void;
+  handleUserInfo: (data: SignInResponse) => void;
+}
+
+const AuthHome = ({ handleStatusCode, handleUserInfo }: IAuthHome) => {
+  const kakaoOnSuccess = async (resData: { response: LoginResponse }) => {
+    const idToken = resData.response.access_token;
+
+    try {
+      const { status, data }: AuthResponse = await getAuth('kakao', idToken);
+      handleStatusCode(status);
+
+      if (status === 201) handleUserInfo(data);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(
+          'There has been a problem with your fetch operation: ',
+          error.message,
+        );
+      }
+    }
+  };
+
+  const googleOnSuccess = async (idToken: string) => {
+    try {
+      const { status, data }: AuthResponse = await getAuth('google', idToken);
+      handleStatusCode(status);
+
+      if (status === 201) handleUserInfo(data);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(
+          'There has been a problem with your fetch operation: ',
+          error.message,
+        );
+      }
+    }
+  };
+
+  return (
+    <section className="mt-[2.3rem] w-full">
+      <div className="flex flex-col items-center">
+        <h1 className="mb-4 text-lg font-semibold">
+          즐거운 배움으로 이어주는 연결고리
+        </h1>
+        <ConnectionLogoSVG className="scale-75" />
+      </div>
+
+      <div className="mb-6 mt-12 flex w-full items-center ">
+        <div className="flex h-[1px] w-full flex-1 border-t border-solid border-sub-color2" />
+        <span className="mx-3 whitespace-nowrap text-sm text-sub-color2">
+          소셜로그인으로 간편하게 시작하기
+        </span>
+        <div className="flex h-[1px] w-full flex-1  border-t border-solid border-sub-color2" />
+      </div>
+
+      <div className="mb-[2.13rem] flex items-center justify-center gap-6">
+        <KakaoAuth
+          onSuccess={kakaoOnSuccess}
+          onFail={(error) => {
+            console.error('로그인 실패:', error);
+            // --- 오류 토스트 메시지 표시 예정 ---
+          }}
+          useLoginForm
+        />
+        {GOOGLE_CLIENT_ID && (
+          <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+            <GoogleAuth
+              onSuccess={googleOnSuccess}
+              onError={(error) => {
+                console.error('로그인 실패:', error);
+                // --- 오류 토스트 메시지 표시 예정 ---
+              }}
+            />
+          </GoogleOAuthProvider>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default AuthHome;
