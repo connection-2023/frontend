83db004a2476142faf1c5d70f35abbcdc540fa4a
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-10-09 19:38:43 +0900

    feat(#81): 로그인/회원가입 모달 및 가입 화면 구현

--- src/components/Header.tsx/UserProfileLinks.tsx
+++ src/components/Header.tsx/UserProfileLinks.tsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import Profile from './Profile';
 import { dummyUserInfo } from '@/constants/dummy';
 import { AlarmSVG, CommentSVG, SearchSVG } from '../../../public/icons/svg';
+import AuthModal from './Auth/AuthModal';
 
 const UserProfileLinks = () => {
   const { alarmCount, commentCount } = dummyUserInfo;
+  const [isOpened, setIsOpened] = useState(false);
+
+  const handleOpened = () => {
+    setIsOpened(true);
+  };
+
+  const handleClosed = () => {
+    setIsOpened(false);
+  };
+
   return (
     <div className="flex items-end gap-3">
       <h2 className="text-0 overflow-hidden indent-[-9999px]">
@@ -15,7 +27,12 @@ const UserProfileLinks = () => {
         <SearchSVG className="h-[1.8rem] w-[1.8rem]" />
       </Link>
 
-      <button className="relative">
+      <div className="text-lg">
+        <button onClick={handleOpened}>로그인/회원가입</button>
+      </div>
+
+      {isOpened && <AuthModal isOpened={isOpened} isClosed={handleClosed} />}
+      {/* { <button className="relative">
         <AlarmSVG className="pt-0.5" />
         <span className="absolute -right-1.5 top-0 min-w-[1rem] rounded-full bg-main-color px-1 text-xs font-bold text-white">
           {alarmCount}
@@ -29,7 +46,7 @@ const UserProfileLinks = () => {
         </span>
       </button>
 
-      <Profile />
+      <Profile />} */}
     </div>
   );
 };
