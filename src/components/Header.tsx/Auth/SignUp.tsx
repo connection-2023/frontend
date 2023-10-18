7dcadd16500712b70c232406b6bff5763872ca00
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-10-19 03:15:06 +0900

    feat(#98): 회원가입 관련 로직 추가

--- src/components/Header.tsx/Auth/SignUp.tsx
+++ src/components/Header.tsx/Auth/SignUp.tsx
@@ -1,24 +1,76 @@
 import { useState } from 'react';
+import Link from 'next/link';
+import { toast } from 'react-toastify';
 import UserInfoSetup from './UserInfoSetup';
 import ProfileSetup from './ProfileSetup';
 import Welcome from './Welcome';
-import { SignInResponse } from '@/types/auth';
+import { SignInResponse, ISignUp } from '@/types/auth';
 
-interface ISignUp {
+interface SignUpProps {
   userInfo: SignInResponse;
   onClickPrev: () => void;
 }
 
-const SignUp = ({ userInfo, onClickPrev }: ISignUp) => {
+const SignUp = ({ userInfo, onClickPrev }: SignUpProps) => {
+  const [userSignUp, setUserSignUp] = useState({
+    name: '',
+    nickname: '',
+    email: userInfo.authEmail,
+    provider: userInfo.signUpType,
+    authEmail: userInfo.authEmail,
+  });
   const [step, setStep] = useState(0);
+  const [userNickname, setUserNickname] = useState('');
+
+  const handleUserInput = (key: keyof ISignUp, value: string | File) => {
+    // 예외 처리 필요
+    setUserSignUp((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
   const Views = [
-    <UserInfoSetup key="userInfo" userInfo={userInfo} />,
-    <ProfileSetup key="profile" defaultProfile={null} />,
-    <Welcome key="welcome" nickname="닉네임" />,
+    <UserInfoSetup
+      key="userInfo"
+      userInfo={userSignUp}
+      handleUserInfo={handleUserInput}
+    />,
+    <Welcome key="welcome" nickname={userNickname} />,
+    <ProfileSetup
+      key="profile"
+      defaultProfile={null}
+      handleUserInfo={handleUserInput}
+    />,
   ];
 
-  const handleNextStep = () => {
-    setStep((prev) => prev + 1);
+  const handleNextStep = async () => {
+    if (step === 0) {
+      console.log(userSignUp);
+
+      const res = await fetch('api/auth', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userSignUp),
+      });
+
+      const { statusCode, data } = await res.json();
+
+      if (statusCode === 201) {
+        setUserNickname(data.createUser.nickname);
+        setStep((prev) => prev + 1);
+        // 로그인 처리하기
+      } else if (statusCode === 400) {
+        toast.error('사용 중인 이메일입니다.');
+      }
+    } else if (step === 1) {
+      // 프로필 사진 등록하기로 가기
+      setStep((prev) => prev + 1);
+    } else if (step === 2) {
+      // 프로필 사진 전송
+    }
   };
 
   const handlePrevStep = () => {
@@ -32,23 +84,25 @@ const SignUp = ({ userInfo, onClickPrev }: ISignUp) => {
       {/* 해당 View 보여주기 */}
       {Views[step]}
 
-      {step < 2 && (
-        <div className="mb-4 flex h-8 w-full justify-between gap-4 px-[1.31rem] text-base font-semibold">
-          <button
-            onClick={handlePrevStep}
-            className="max-w-48 w-full rounded-[0.31rem] bg-sub-color2"
-          >
-            이전
-          </button>
-          {/* 백엔드 API 전송 */}
-          <button
-            onClick={handleNextStep}
-            className="max-w-48 w-full rounded-[0.31rem] bg-sub-color1 text-white"
-          >
-            다음
-          </button>
-        </div>
-      )}
+      <div className="mb-4 flex h-8 w-full justify-between gap-4 px-[1.31rem] text-base font-semibold">
+        <button
+          onClick={handlePrevStep}
+          className="max-w-48 h-12 w-full rounded-[0.31rem] border border-solid border-sub-color2 text-sub-color1"
+        >
+          {step === 0 && '이전'}
+          {step === 1 && <Link href="/class">클래스 보러가기</Link>}
+          {step === 2 && '건너뛰기'}
+        </button>
+
+        <button
+          onClick={handleNextStep}
+          className="max-w-48 h-12 w-full rounded-[0.31rem] bg-sub-color1 text-white"
+        >
+          {step === 0 && '가입완료'}
+          {step === 1 && '프로필사진 등록'}
+          {step === 2 && '저장하기'}
+        </button>
+      </div>
     </>
   );
 };
