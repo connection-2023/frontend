708f10ea1511dbd420240a8dda15d3e36b7fb4c8
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-10-18 17:55:04 +0900

    feat: focus ouline 스타일 추가

--- src/components/TextArea/TextAreaSection.tsx
+++ src/components/TextArea/TextAreaSection.tsx
@@ -54,7 +54,7 @@ const TextAreaSection = ({
           ...(isRequired && { required: errorMessage }),
           maxLength,
         })}
-        className={`${height} resize-none rounded-md border border-sub-color2 p-3`}
+        className={`${height} resize-none rounded-md border border-sub-color2 p-3 focus:outline-sub-color1`}
         placeholder={placeholder}
         defaultValue={defaultValue}
         maxLength={maxLength}
