fffe0af53063ef20b57c8f9b70ef5582588a88e1
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-10-29 06:24:49 +0900

    feat(#112): 에러 처리 추가

--- src/lib/apis/instructorPostApis.ts
+++ src/lib/apis/instructorPostApis.ts
@@ -1,9 +1,13 @@
 import { DOMAIN } from '@/constants/constants';
 
 export const getInstructorPost = async (id: string) => {
-  const response = await fetch(`${DOMAIN}/api/post/instructor?id=${id}`, {
-    method: 'GET',
-  }).then((data) => data.json());
+  try {
+    const response = await fetch(`${DOMAIN}/api/post/instructor?id=${id}`, {
+      method: 'GET',
+    }).then((data) => data.json());
 
-  return response.data.lecturerProfile;
+    return response.data.lecturerProfile;
+  } catch (error) {
+    return new Error('잘못된 요청입니다!');
+  }
 };
