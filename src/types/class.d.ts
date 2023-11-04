5d34404b0d67a05fa10099b36222a924a3e2cc7d
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-11-05 02:48:48 +0900

    feat(#128): 클래스 상세조회 데이터 타입 추가

--- src/types/class.d.ts
+++ src/types/class.d.ts
@@ -1,3 +1,4 @@
+import { IRegion, IGenre } from './types';
 export interface ClassCardType {
   status: '모집중' | '마감임박' | '마감';
   date: string;
@@ -18,13 +19,92 @@ export interface Space {
   total: number;
 }
 
-export interface DateTime {
-  date: string;
-  space: Space;
+export interface IDateTime {
   count: number;
+  dateTime: string;
+  lectureId?: number;
+  lectureScheduleId: number;
+  space: Space;
 }
 
 export interface DayTimeList {
   day: string[];
   timeSlots: string[];
+}
+
+export interface IClassSchedule {
+  id: number;
+  lectureId: number;
+  startDateTime: string;
+  numberOfParticipants: number;
+  team: null | string;
+}
+
+export interface IClassPostResponse {
+  id: number;
+  lecturerId: number;
+  lectureTypeId: number;
+  lectureMethodId: number;
+  isGroup: boolean;
+  title: string;
+  introduction: string;
+  curriculum: string;
+  detailAddress: string;
+  duration: number;
+  difficultyLevel: string;
+  minCapacity: number;
+  maxCapacity: number;
+  reservationDeadline: number;
+  reservationComment: string;
+  price: number;
+  noShowDeposit: null;
+  reviewCount: number;
+  stars: number;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: null | string;
+  lectureType: {
+    name: string;
+  };
+  lectureMethod: {
+    name: string;
+  };
+  lectureReview: IUserReview[];
+  lectureNotification: {
+    notification: string;
+  };
+  lectureImage: IImage[];
+  lectureCouponTarget: any; // 변경 예정
+  lectureSchedule: IClassSchedule[];
+  lectureHoliday: IHoilday[];
+  lectureToRegion: IRegion[];
+  lectureToDanceGenre: IGenre[];
+
+  lecturer: IInstructorProfile;
+}
+
+interface IInstructorProfile {
+  id: number;
+  profileCardImageUrl: null | string;
+  nickname: string;
+}
+
+interface IHoilday {
+  holiday: Date | string;
+}
+
+interface IImage {
+  imageUrl: string;
+}
+
+interface IUserReview {
+  id: number;
+  userId: number;
+  users: {
+    nickname: string;
+    userProfileImage: null | string;
+  };
+  stars: number;
+  description: string;
 }
