d2768e5a7b1519836113237f4012dfcfcbceb527
hyeonjun-L <113358733+hyeonjun-L@users.noreply.github.com>
2023-11-06 18:14:09 +0000

    Deploying to gh-pages from @ connection-2023/connection-frontend@87ade875585aa44ab591d0a272d6fcde30e15167 🚀

---
+++ storybook/icons/svg.ts
@@ -0,0 +1,147 @@
+import AddImageSVG from './addimage.svg';
+import AlarmSVG from './alarm.svg';
+import ApplySuccessSVG from './applySuccess.svg';
+import Arrow from './arrow.svg';
+import ArrowDownSVG from './ArrowDown.svg';
+import ArrowRightSVG from './ArrowRight.svg';
+import ArrowUpSVG from './ArrowUp.svg';
+import AuthWelcomeSVG from './authWelcome.svg';
+import BasicCalendarSVG from './BasicCalendar.svg';
+import BigArrowSVG from './bigArrow.svg';
+import BillSVG from './bill.svg';
+import BookmarkSVG from './bookmark.svg';
+import CalendarSVG from './calendar.svg';
+import CalendarDetailSVG from './calendarDetail.svg';
+import ChatSVG from './chat.svg';
+import CheckSVG from './check.svg';
+import CheckMarkSVG from './checkMark.svg';
+import ClassStatusSVG from './classStatus.svg';
+import ClearSVG from './clear.svg';
+import CloseSVG from './close.svg';
+import CommentSVG from './comment.svg';
+import ConnectionLogoSVG from './connectionLogo.svg';
+import CouponSVG from './coupon.svg';
+import CropSVG from './crop.svg';
+import DashboardSVG from './dashboard.svg';
+import DeleteSVG from './delete.svg';
+import DoubleRightSVG from './doubleRight.svg';
+import DownloadSVG from './download.svg';
+import EditSVG from './edit.svg';
+import ExcelSVG from './excel.svg';
+import FacebookSVG from './facebook.svg';
+import GoogleSVG from './googleIcon.svg';
+import GroupSVG from './group.svg';
+import HeartSVG from './heart.svg';
+import ImageSVG from './image.svg';
+import InstagramSVG from './instagram.svg';
+import KaKaoTalkSVG from './kakaoTalk.svg';
+import LevelSVG from './level.svg';
+import LikeSVG from './like.svg';
+import LinkSVG from './link.svg';
+import LocationSVG from './location.svg';
+import MainPopularSVG from './mainPopular.svg';
+import MainTopSVG from './mainTop.svg';
+import MapSVG from './map.svg';
+import MoneySVG from './money.svg';
+import MusicalNoteSVG from './musicalNote.svg';
+import NaverSVG from './naverIcon.svg';
+import NoteSVG from './note.svg';
+import NotFoundSVG from './notFound.svg';
+import NoticeSVG from './notice.svg';
+import OptionSVG from './option.svg';
+import PlusesSVG from './pluses.svg';
+import ProfileSVG from './profile_default.svg';
+import ReadMessageSVG from './readMessage.svg';
+import RecentSVG from './recent.svg';
+import ReportSVG from './report.svg';
+import ResetSVG from './reset.svg';
+import ReviewSVG from './review.svg';
+import SearchSVG from './search.svg';
+import ShareSVG from './share.svg';
+import SmallLogoSVG from './smallLogo.svg';
+import StarSVG from './star.svg';
+import TimeSVG from './time.svg';
+import TooltipSVG from './tooltip.svg';
+import TransFormSVG from './transform.svg';
+import TwitterSVG from './twitter.svg';
+import UnreadMessageSVG from './unreadMessage.svg';
+import UploadImageSVG from './UploadImg.svg';
+import WavyLineSVG from './wavyLine.svg';
+import YoutubeSVG from './youtube.svg';
+import ZoomInSVG from './zoomIn.svg';
+import ZoomOutSVG from './zoomOut.svg';
+
+export {
+  ApplySuccessSVG,
+  ClassStatusSVG,
+  ReviewSVG,
+  RecentSVG,
+  DeleteSVG,
+  WavyLineSVG,
+  UnreadMessageSVG,
+  ReadMessageSVG,
+  DashboardSVG,
+  DownloadSVG,
+  NotFoundSVG,
+  ReportSVG,
+  BookmarkSVG,
+  LikeSVG,
+  CouponSVG,
+  TooltipSVG,
+  PlusesSVG,
+  CheckMarkSVG,
+  AuthWelcomeSVG,
+  NaverSVG,
+  GoogleSVG,
+  ImageSVG,
+  AddImageSVG,
+  TransFormSVG,
+  CommentSVG,
+  ExcelSVG,
+  MoneySVG,
+  ConnectionLogoSVG,
+  CalendarDetailSVG,
+  SmallLogoSVG,
+  InstagramSVG,
+  YoutubeSVG,
+  LinkSVG,
+  OptionSVG,
+  ShareSVG,
+  CalendarSVG,
+  HeartSVG,
+  StarSVG,
+  ProfileSVG,
+  ResetSVG,
+  ArrowUpSVG,
+  ArrowDownSVG,
+  Arrow,
+  SearchSVG,
+  CloseSVG,
+  ClearSVG,
+  ChatSVG,
+  GroupSVG,
+  LevelSVG,
+  LocationSVG,
+  NoticeSVG,
+  TimeSVG,
+  BasicCalendarSVG,
+  ArrowRightSVG,
+  UploadImageSVG,
+  CropSVG,
+  ZoomInSVG,
+  ZoomOutSVG,
+  CheckSVG,
+  KaKaoTalkSVG,
+  TwitterSVG,
+  FacebookSVG,
+  MainPopularSVG,
+  MainTopSVG,
+  MapSVG,
+  AlarmSVG,
+  BigArrowSVG,
+  EditSVG,
+  DoubleRightSVG,
+  BillSVG,
+  NoteSVG,
+  MusicalNoteSVG,
+};
