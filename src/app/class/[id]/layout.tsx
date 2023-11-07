0736994a6ed0660c22c2fc8b4223b1b24973b71b
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-11-07 23:44:28 +0900

    feat(#128): dynamic 메타 데이터로 변경

--- src/app/class/[id]/layout.tsx
+++ src/app/class/[id]/layout.tsx
@@ -1,11 +1,28 @@
 import type { Metadata } from 'next';
+import { getClassPost } from '@/lib/apis/classApis';
 
-export const metadata: Metadata = {
-  title: 'Connection | 강의 상세페이지',
-  description: 'Connection 강의 상세페이지',
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> => {
+  const { id } = params;
+
+  const classData = await getClassPost(id);
+  if (classData instanceof Error) {
+    return {
+      title: 'Connection | 강의 상세페이지',
+      description: 'Connection 강의 상세페이지',
+    };
+  }
+
+  return {
+    title: `Connection | ${classData.title}`,
+    description: classData.introduction,
+  };
 };
 
-export default function ClassLayout({
+export default function ClassDetailLayout({
   children,
 }: {
   children: React.ReactNode;
