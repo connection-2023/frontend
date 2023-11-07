fd8c12a029717baed8b56ed4b848b575f61741ca
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-11-07 23:45:17 +0900

    feat(#128): 날짜 파싱 함수 추가

--- src/utils/parseUtils.ts
+++ src/utils/parseUtils.ts
@@ -2,6 +2,7 @@ import { format, addMinutes } from 'date-fns';
 import { ko } from 'date-fns/locale';
 import { IGenre, IRegion } from '@/types/types';
 
+// 서울 특별시 -> 서울 충청북도 -> 충북
 export const formatLocationToString = (
   regions: { region: IRegion }[] | IRegion[],
 ) =>
@@ -33,4 +34,8 @@ export const formatDateTime = (datetime: Date, duration: number) => {
   });
   const formattedEndDatetime = format(endDatetime, 'HH:mm');
   return `${formattedStartDatetime}-${formattedEndDatetime}`;
+};
+
+export const formatDate = (date: string) => {
+  return format(new Date(date), 'MM/dd');
 };
