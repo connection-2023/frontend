e8b2fbaec6bbcfa6bb7d3307437533db9e713c39
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-11-09 23:05:40 +0900

    feat: 닫기 버튼 onClick 이벤트 추가

--- src/components/Modal/RouterModal.tsx
+++ src/components/Modal/RouterModal.tsx
@@ -22,14 +22,17 @@ const Modal = ({ children }: { children: React.ReactNode }) => {
   return (
     <div
       ref={overlayRef}
-      className="z-modal fixed bottom-0 left-0 right-0 top-0 mx-auto bg-black/60 backdrop-blur-sm"
+      className="fixed bottom-0 left-0 right-0 top-0 z-modal mx-auto bg-black/60 backdrop-blur-sm"
       onClick={(e) => {
         if (overlayRef.current !== e.target) return;
         router.back();
       }}
     >
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md bg-white py-4 shadow-float">
-        <button className="absolute right-2 top-2">
+        <button
+          onClick={() => router.back()}
+          className="absolute right-2 top-2"
+        >
           <CloseSVG
             width="24"
             height="24"
