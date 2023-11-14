fa1f4e64b4076509f94582190f7a68c72274d938
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-11-14 11:24:54 +0900

    feat(#144): 회원가입/로그인 intercepting route 대체

---
+++ src/app/@modal/(.)register/page.tsx
@@ -0,0 +1,164 @@
+'use client';
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import UserSetup from '@/app/(auth)/register/_components//UserSetup';
+import UserConsentForm from '@/app/(auth)/register/_components/UserConsentForm';
+import { getAuth, postUserRegister, getMyProfile } from '@/lib/apis/userApi';
+import { useUserStore } from '@/store';
+import RouterModal from '@/components/Modal/RouterModal';
+import { IRegisterConsents, SignInResponse } from '@/types/auth';
+import { IRegisterForm } from '@/types/form';
+
+const RegisterModal = () => {
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+  const userEmail = searchParams.get('userEmail');
+  const signUpType = searchParams.get('type');
+  const store = useUserStore();
+  const router = useRouter();
+  const [activeStep, setActiveStep] = useState(0);
+  const [userRegistrationForm, setUserRegistrationForm] =
+    useState<IRegisterForm>();
+  const [userConsents, setUserConsents] = useState<IRegisterConsents>();
+
+  if (!token || !userEmail || !signUpType) return;
+
+  const isValidSignUpType = (
+    value: string,
+  ): value is SignInResponse['signUpType'] => {
+    return value === 'NAVER' || value === 'KAKAO' || value === 'GOOGLE';
+  };
+
+  if (!isValidSignUpType(signUpType)) {
+    return;
+  }
+
+  const handleNextStep = () => {
+    if (activeStep < 1) {
+      setActiveStep((prev) => prev + 1);
+    }
+  };
+
+  const handleUserInfo = (data: IRegisterForm) => {
+    setUserRegistrationForm(data);
+    handleNextStep();
+  };
+
+  const handleUserAgree = (data: IRegisterConsents) => {
+    setUserConsents(data);
+    handleNextStep();
+  };
+
+  const handleUserRegister = async () => {
+    if (!userRegistrationForm) return;
+
+    const { name, nickname, phoneNumber } = userRegistrationForm;
+
+    const userInfo = {
+      provider: signUpType,
+      email: userEmail,
+      authEmail: userEmail,
+      name,
+      nickname,
+      ...(phoneNumber ? { phoneNumber } : {}),
+    };
+
+    const response = await postUserRegister(userInfo);
+    const { statusCode, data } = response;
+
+    if (statusCode === 201) {
+      const response = await getAuth(signUpType, token).then((res) =>
+        getMyProfile(res.data.userAccessToken),
+      );
+
+      store.setAuthUser(response.data.myProfile);
+      router.replace(`/welcome/${data.createUser.nickname}`);
+    } else if (statusCode === 400) {
+      toast.error('사용 중인 이메일입니다.');
+      router.replace('/');
+    }
+  };
+
+  const updateUserRegistrationData = (
+    data?: IRegisterForm | IRegisterConsents,
+  ) => {
+    if (data) {
+      switch (activeStep) {
+        case 0:
+          handleUserInfo(data as IRegisterForm);
+          break;
+        case 1:
+          handleUserAgree(data as IRegisterConsents);
+          handleUserRegister();
+          break;
+        default:
+          console.error('Invalid step');
+      }
+    }
+  };
+
+  const handlePrev = () => {
+    if (activeStep === 1) {
+      setActiveStep((prev) => prev - 1);
+    }
+  };
+
+  const steps = [
+    {
+      title: '회원정보 입력',
+      component: (
+        <UserSetup
+          userEmail={userEmail}
+          signUpType={signUpType}
+          userRegistrationForm={userRegistrationForm}
+          updateUserRegistrationData={updateUserRegistrationData}
+        />
+      ),
+    },
+    {
+      title: '약관동의',
+      component: (
+        <UserConsentForm
+          handlePrev={handlePrev}
+          updateUserRegistrationData={updateUserRegistrationData}
+        />
+      ),
+    },
+  ];
+
+  return (
+    <RouterModal>
+      <section className="mb-2 flex h-[37.5rem] w-full flex-col rounded-md bg-white px-6">
+        <ul className="mb-6 mt-[3.5rem] flex h-[35px] shrink-0 items-center justify-between whitespace-nowrap rounded-[3.13rem] text-base font-semibold shadow-float">
+          {steps.map((step, index) => (
+            <li
+              key={index}
+              className={`flex h-full flex-grow items-center justify-center gap-2 rounded-[3.13rem] px-1 ${
+                activeStep === index
+                  ? 'bg-sub-color1 text-white'
+                  : 'text-gray-500'
+              }`}
+            >
+              <span
+                className={`flex h-[19px] w-[19px] items-center justify-center rounded-full text-base ${
+                  activeStep === index
+                    ? 'bg-white text-sub-color1'
+                    : 'bg-gray-500 text-white'
+                }`}
+              >
+                {index + 1}
+              </span>
+              {step.title}
+            </li>
+          ))}
+        </ul>
+
+        {steps[activeStep].component}
+      </section>
+    </RouterModal>
+  );
+};
+
+export default RegisterModal;
