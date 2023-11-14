95a869fc1628e3f483822d35e246e65b5da6a4bd
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-11-14 11:27:41 +0900

    feat(#133): 모달 컴포넌트 isOpened, handleClosed props 추가

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,48 +1,48 @@
 'use client';
-import { useRouter } from 'next/navigation';
-import React from 'react';
+import { useEffect, useRef } from 'react';
 import { CloseSVG } from '@/icons/svg';
 
-const Modal = ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter();
-  const overlayRef = React.useRef(null);
+interface ModalProps {
+  children: React.ReactNode;
+  isOpened: boolean;
+  handleClosed: () => void;
+}
+const Modal = ({ children, isOpened, handleClosed }: ModalProps) => {
+  const overlayRef = useRef(null);
 
   const handleKeyUp = (e: KeyboardEvent) => {
     if (e.key !== 'Escape') return;
-
-    router.back();
+    handleClosed();
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     window.addEventListener('keyup', handleKeyUp);
 
     return () => window.removeEventListener('keyup', handleKeyUp);
   }, []);
 
-  return (
+  return isOpened ? (
     <div
       ref={overlayRef}
-      className="fixed bottom-0 left-0 right-0 top-0 z-modal mx-auto bg-black/60"
+      className="fixed bottom-0 left-0 right-0 top-0 z-modal mx-auto bg-black/60 backdrop-blur-sm"
       onClick={(e) => {
         if (overlayRef.current !== e.target) return;
-
-        router.back();
+        handleClosed();
       }}
     >
-      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="flex justify-end">
-          <button onClick={() => router.back()}>
-            <CloseSVG
-              width={24}
-              height={24}
-              className="stroke-gray-500 stroke-2"
-            />
-          </button>
-        </div>
+      <div className="absolute left-1/2 top-1/2 h-auto w-auto -translate-x-1/2 -translate-y-1/2 rounded-md bg-white shadow-float">
+        <button onClick={handleClosed} className="absolute right-2 top-2">
+          <CloseSVG
+            width="24"
+            height="24"
+            className="stroke-gray-500 stroke-2"
+          />
+        </button>
+
         {children}
       </div>
     </div>
-  );
+  ) : null;
 };
 
 export default Modal;
