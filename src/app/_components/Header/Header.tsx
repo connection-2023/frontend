4f3c271b53e82e6f26b1e34acd5114c27e5b0dae
hyeonjun-L <beeminor60@gmail.com>
2023-11-15 21:37:25 +0900

    feat(#148): 헤더 컴포넌트 스크롤 방향에 따른 반응 구현

--- src/app/_components/Header/Header.tsx
+++ src/app/_components/Header/Header.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { usePathname } from 'next/navigation';
+import { useEffect, useRef, useState } from 'react';
 import {
   NON_STICKY_HEADER_PATHS,
   NO_HEADER_FOOTER_PATHS,
@@ -9,11 +10,34 @@ import UserProfileLinks from './UserProfileLinks';
 
 const Header = () => {
   const pathname = usePathname();
+  const lastScrollTopRef = useRef(0);
+  const [isScrollingUp, setIsScrollingUp] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+      if (currentScrollTop <= 100) {
+        setIsScrollingUp(true);
+      } else {
+        setIsScrollingUp(lastScrollTopRef.current > currentScrollTop);
+      }
+
+      lastScrollTopRef.current = currentScrollTop;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   const getHeaderStyle = (isStickyHeader: boolean) => {
-    const baseStyle =
-      'top-0 z-header pb-3 px-4 mx-auto flex h-[5.6rem] md:h-[7.6rem] w-screen items-end justify-between bg-white/[.95] md:px-16 md:pb-5 backdrop-blur-sm';
-    return isStickyHeader ? `${baseStyle} relative` : `${baseStyle} sticky`;
+    const baseStyle = `${
+      isScrollingUp
+        ? 'translate-y-0 duration-[300ms]'
+        : '-translate-y-full duration-[300ms]'
+    } top-0 z-header pb-3 px-4 mx-auto flex h-[5.6rem] md:h-[7.6rem] w-screen items-end justify-between bg-white/[.95] md:px-16 md:pb-5 backdrop-blur-sm `;
+    return isStickyHeader ? `${baseStyle} relative ` : `${baseStyle} sticky`;
   };
 
   const isPathInList = (pathList: string[]) =>
