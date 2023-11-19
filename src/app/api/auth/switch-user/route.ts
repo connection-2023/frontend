0405a5ddb8cb4049dd2c4af6539dd6cefc5600d3
hyeonjun-L <beeminor60@gmail.com>
2023-11-19 21:30:30 +0900

    refactor(#148): 유저 전환, 강사 전환 refresh 토큰 전환 구현

--- src/app/api/auth/switch-user/route.ts
+++ src/app/api/auth/switch-user/route.ts
@@ -32,38 +32,54 @@ export const GET = async (request: NextRequest) => {
     const serverResponse = await fetch(END_POINT + requestUrl, {
       credentials: 'include',
       headers,
-    }).then((res) => res.json());
-
-    const clientResponse = new NextResponse(
-      JSON.stringify({ status: serverResponse.statusCode }),
-    );
-
-    if (serverResponse.statusCode === 200) {
-      if (tokenName === 'userAccessToken') {
-        await Promise.all([
-          clientResponse.cookies.set({
-            name: 'lecturerAccessToken',
-            value: serverResponse.data.lecturerAccessToken,
-            httpOnly: true,
-            path: '/',
-            secure: process.env.NODE_ENV !== 'development',
-          }),
-          clientResponse.cookies.delete('userAccessToken'),
-        ]);
-      } else if (tokenName === 'lecturerAccessToken') {
-        await Promise.all([
-          clientResponse.cookies.set({
-            name: 'userAccessToken',
-            value: serverResponse.data.userAccessToken,
-            httpOnly: true,
-            path: '/',
-            secure: process.env.NODE_ENV !== 'development',
-          }),
-          clientResponse.cookies.delete('lecturerAccessToken'),
-        ]);
+    }).then(async (res) => {
+      if (!res.ok) {
+        throw new Error('HTTP error ' + res.status);
       }
-    }
-    return clientResponse;
+
+      const { statusCode, data } = await res.json();
+
+      const clientResponse = new NextResponse(
+        JSON.stringify({ status: statusCode }),
+        {
+          status: statusCode,
+          headers: {
+            'Content-Type': 'application/json',
+            'Set-Cookie': res.headers.get('Set-Cookie') || '',
+          },
+        },
+      );
+
+      if (statusCode === 200) {
+        if (tokenName === 'userAccessToken') {
+          await Promise.all([
+            clientResponse.cookies.set({
+              name: 'lecturerAccessToken',
+              value: data.lecturerAccessToken,
+              httpOnly: true,
+              path: '/',
+              secure: process.env.NODE_ENV !== 'development',
+            }),
+            clientResponse.cookies.delete('userAccessToken'),
+          ]);
+        } else if (tokenName === 'lecturerAccessToken') {
+          await Promise.all([
+            clientResponse.cookies.set({
+              name: 'userAccessToken',
+              value: data.userAccessToken,
+              httpOnly: true,
+              path: '/',
+              secure: process.env.NODE_ENV !== 'development',
+            }),
+            clientResponse.cookies.delete('lecturerAccessToken'),
+          ]);
+        }
+      }
+
+      return clientResponse;
+    });
+
+    return serverResponse;
   } catch (error) {
     return new NextResponse('유저 전환 네트워크 요청 에러: ', { status: 500 });
   }
