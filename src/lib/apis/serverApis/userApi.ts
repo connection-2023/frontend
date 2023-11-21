f2c01e05e39fb072155117333e1be918d7c06a04
hyeonjun-L <beeminor60@gmail.com>
2023-11-21 17:22:16 +0900

    feat(#148): 엑세스 토큰 재발급 서버 api 구현

--- src/lib/apis/serverApis/userApi.ts
+++ src/lib/apis/serverApis/userApi.ts
@@ -1,4 +1,6 @@
 import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+import { DOMAIN } from '@/constants/constants';
 import { instructorProfile, userProfile } from '@/types/auth';
 
 const END_POINT = process.env.NEXT_PUBLIC_API_END_POINT;
@@ -18,7 +20,12 @@ export const getInstructorProfile = async (): Promise<instructorProfile> => {
   });
 
   if (!response.ok) {
-    throw new Error(`Server error: ${response.status}`);
+    const errorData = await response.json();
+    throw new Error(
+      `강사 프로필 조회 에러: ${errorData.message || ''}, status: ${
+        response.status
+      }`,
+    );
   }
 
   const data = await response.json();
@@ -41,10 +48,85 @@ export const getMyProfile = async (): Promise<userProfile> => {
   });
 
   if (!response.ok) {
-    throw new Error(`Server error: ${response.status}`);
+    const errorData = await response.json();
+    throw new Error(
+      `유저 프로필 조회 에러: ${errorData.message || ''}, status: ${
+        response.status
+      }`,
+    );
   }
 
   const data = await response.json();
 
   return data.data.myProfile;
+};
+
+export const accessTokenReissuance = async (): Promise<{
+  accessToken: string;
+  refreshToken: string;
+}> => {
+  const cookieStore = cookies();
+  const refreshToken = cookieStore.get('refreshToken')?.value;
+
+  const response = await fetch(END_POINT + '/auth/token/refresh', {
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      Cookie: `refreshToken=${refreshToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(
+      `refresh 조회 에러: ${errorData.message || ''}, status: ${
+        response.status
+      }`,
+    );
+  }
+
+  const resCookies = response.headers.get('set-cookie')?.split('; ');
+  const refreshTokenCookie = resCookies!.find((cookie) =>
+    cookie.startsWith('refreshToken='),
+  );
+  const data = await response.json();
+  const userAccessToken = data.data.userAccessToken;
+  const lecturerAccessToken = data.data.lecturerAccessToken;
+  const resRefreshToken = refreshTokenCookie!.split('=')[1];
+
+  if (userAccessToken) {
+    return { accessToken: userAccessToken, refreshToken: resRefreshToken };
+  } else if (lecturerAccessToken) {
+    return { accessToken: lecturerAccessToken, refreshToken: resRefreshToken };
+  } else {
+    throw new Error('엑세스 토큰 미 발급');
+  }
+};
+
+export const checkAccessToken = async (
+  tokenType: 'userAccessToken' | 'lecturerAccessToken',
+) => {
+  const cookieStore = cookies();
+  const authorization = cookieStore.get(tokenType)?.value;
+  const point =
+    tokenType === 'userAccessToken'
+      ? 'user-access-token'
+      : 'lecturer-access-token';
+
+  const headers: Record<string, string> = {
+    Authorization: `Bearer ${authorization}`,
+  };
+
+  const response = await fetch(`${END_POINT}/auth/token/verify/${point}`, {
+    method: 'GET',
+    credentials: 'include',
+    headers,
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(
+      `토큰 검증 에러: ${errorData.message || ''}, status: ${response.status}`,
+    );
+  }
 };
