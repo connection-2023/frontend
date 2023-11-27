28eddea95135039638afe41bf2c57b675e466462
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-11-27 22:09:55 +0900

    feat(#147): 영수증 조회 api 로직 추가

--- src/lib/apis/serverApis/paymentsApis.ts
+++ src/lib/apis/serverApis/paymentsApis.ts
@@ -1,11 +1,15 @@
-import { IPaymentConfirmRequest } from '@/types/payment';
+import {
+  IPaymentConfirmRequest,
+  IPaymentConfirmResponse,
+  IReceiptResponse,
+} from '@/types/payment';
 
 const END_POINT = process.env.NEXT_PUBLIC_API_END_POINT;
 
 export const patchPaymentConfirm = async (
   token: string,
   data: IPaymentConfirmRequest,
-) => {
+): Promise<IPaymentConfirmResponse | Error> => {
   try {
     const headers: Record<string, string> = {
       'Content-Type': 'application/json',
@@ -19,9 +23,34 @@ export const patchPaymentConfirm = async (
       body: JSON.stringify(data),
     }).then((data) => data.json());
 
-    return response.data.paymentResult;
+    return response.data?.paymentResult;
   } catch (error) {
     console.error('결제 승인 오류', error);
+    throw error;
+  }
+};
+
+export const getReceipt = async (
+  token: string,
+  orderId: string,
+): Promise<IReceiptResponse | Error> => {
+  try {
+    const headers: Record<string, string> = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    };
+
+    const response = await fetch(
+      END_POINT + '/payments/user-receipt?orderId=' + orderId,
+      {
+        credentials: 'include',
+        headers,
+      },
+    ).then((data) => data.json());
+
+    return response.data.receipt;
+  } catch (error) {
+    console.error('영수증 요청 오류', error);
     throw error;
   }
 };
