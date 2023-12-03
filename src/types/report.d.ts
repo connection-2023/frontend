02319f890dd2c89ac5a6743be65d09dcbb175550
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-12-04 02:39:52 +0900

    feat(#170): 신고 관련 데이터 분리

---
+++ src/types/report.d.ts
@@ -0,0 +1,19 @@
+export enum ReportType {
+  FALSE_INFORMATION = '허위 정보 기재',
+  COPYRIGHT_INFRINGEMENT = '저작권 불법 도용',
+  INAPPROPRIATE_PHOTO = '부적절한 사진 게시',
+  INAPPROPRIATE_CONTENT = '부적절한 내용',
+  OTHER = '기타',
+}
+
+export type ReportFormData = {
+  [key in ReportType]: boolean;
+} & { reportDetail?: string };
+
+export interface IReportRequest {
+  reportTypes: ReportType[];
+  targetUserId?: number;
+  targetLecturerId?: number;
+  lectureReviewId?: number;
+  reason?: string;
+}
