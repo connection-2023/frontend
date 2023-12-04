dfad4b950f7d2a3cdc0bd15a29be031c36c8175c
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-12-05 03:18:36 +0900

    feat(#169): 클래스 목록 API 연결

--- src/app/my/manage/myclass/page.tsx
+++ src/app/my/manage/myclass/page.tsx
@@ -1,27 +1,61 @@
 'use client';
-import { useState } from 'react';
-import ClassDetailView from './_components/ClassDetailView';
-import ClassListView from './_components/ClassListView';
+import { useEffect, useState } from 'react';
+import { getLecturerClassList } from '@/lib/apis/classApis';
+import ClassList from './_components/ClassList';
+import { ILecturerClassListResonse } from '@/types/class';
 
 const MyClassPage = () => {
-  const [activeView, setActiveView] = useState('list');
+  const [isProgress, setIsProgress] = useState(true);
+  const [classListData, setClassListData] = useState<
+    ILecturerClassListResonse[]
+  >([]);
 
-  const handleItemClick = () => {
-    setActiveView('detail');
+  useEffect(() => {
+    fetchClassListData();
+  }, []);
+
+  const handleOptions = () => {
+    setIsProgress(!isProgress);
   };
 
-  const handleGoBack = () => {
-    setActiveView('list');
+  const fetchClassListData = async () => {
+    const option = isProgress ? '진행중' : '마감된 클래스';
+    const classData = await getLecturerClassList(option);
+    if (classData instanceof Error) return;
+
+    setClassListData(classData);
   };
 
   return (
-    <>
-      {activeView === 'list' ? (
-        <ClassListView onItemClick={handleItemClick} />
-      ) : (
-        <ClassDetailView onGoBack={handleGoBack} />
-      )}
-    </>
+    <section className="col-span-2 h-full w-full flex-col rounded-lg bg-white p-6 shadow-float">
+      <div className="mb-8 flex gap-[2.13rem] text-2xl text-gray-500">
+        <h1
+          onClick={handleOptions}
+          className={`${
+            isProgress
+              ? 'cursor-pointer font-bold text-black'
+              : 'cursor-pointer'
+          }`}
+        >
+          모집/진행중
+        </h1>
+        <h1
+          onClick={handleOptions}
+          className={`${
+            isProgress
+              ? 'cursor-pointer'
+              : 'cursor-pointer font-bold text-black'
+          }`}
+        >
+          마감된 클래스
+        </h1>
+      </div>
+      <ul className="flex flex-col gap-4">
+        {classListData.map((item) => (
+          <ClassList key={item.id} {...item} isProgress={isProgress} />
+        ))}
+      </ul>
+    </section>
   );
 };
 
