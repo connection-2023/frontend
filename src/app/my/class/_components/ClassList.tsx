d7fe9743f8ea6a25a6b8e1e80f9b4d2320eb7bb2
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-12-05 02:11:52 +0900

    feat(#163): 유저 클래스 신청 내역 리스트 API 연결

--- src/app/my/class/_components/ClassList.tsx
+++ src/app/my/class/_components/ClassList.tsx
@@ -1,33 +1,48 @@
+import { format } from 'date-fns';
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { ButtonStyles } from '@/constants/constants';
 import UniqueButton from '@/components/Button/UniqueButton';
 import ProfileImage from '@/components/ProfileImage/ProfileImage';
+import { IUserClassResponse } from '@/types/class';
 
-interface IClassList {
-  activeTab: string;
-  title: string;
-  profile?: {
-    src: null;
-    nickname: string;
-  };
-  onItemClick: () => void;
+interface IClassList extends IUserClassResponse {
+  isProgress: boolean;
 }
 
-const ClassList = ({ activeTab, title, profile, onItemClick }: IClassList) => {
+const ClassList = ({ isProgress, reservation, lecturer }: IClassList) => {
+  const router = useRouter();
+  const classDates = reservation.map((info) => {
+    const date = new Date(info.lectureSchedule.startDateTime);
+
+    return format(date, 'yy.MM.dd');
+  });
+  const uniqueClassDates = new Set(classDates);
+
+  const handleListClick = () => {
+    router.push(`/my/class/${reservation[0].lectureSchedule.lecture.id}`);
+  };
+
+  const handleCurriculumClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    router.push(`/class/${reservation[0].lectureSchedule.lecture.id}`);
+  };
+
   return (
     <li
-      onClick={onItemClick}
-      className="grid h-full max-h-[13.6rem] w-full max-w-[40rem] cursor-pointer auto-rows-max grid-cols-2 gap-x-2.5 gap-y-3.5 rounded-md p-[0.88rem] shadow-vertical md:gap-4"
+      onClick={handleListClick}
+      className="grid h-full max-h-[13.6rem] w-full cursor-pointer auto-rows-max grid-cols-2 gap-x-2.5 gap-y-3.5 rounded-md p-[0.88rem] shadow-vertical md:gap-4"
     >
       <figure className="flex aspect-[297/188] h-full max-h-[188px] w-full md:row-span-2">
         <Image
-          src="https://connection-bucket.s3.amazonaws.com/lectures/1700525913534_99F17695-2B35-4571-85FA-93114EB95768-55389-000021ABFC85CE19-min.JPG"
+          src={reservation[0].lectureSchedule.lecture.lectureImage[0].imageUrl}
           alt="클래스 이미지"
           width={0}
           height={0}
           placeholder="blur"
-          blurDataURL="https://connection-bucket.s3.amazonaws.com/lectures/1700525913534_99F17695-2B35-4571-85FA-93114EB95768-55389-000021ABFC85CE19-min.JPG"
+          blurDataURL={
+            reservation[0].lectureSchedule.lecture.lectureImage[0].imageUrl
+          }
           sizes="(max-width: 720px) 60vw, (max-width: 1440px) 30vw"
           className="object-none"
           style={{
@@ -39,44 +54,41 @@ const ClassList = ({ activeTab, title, profile, onItemClick }: IClassList) => {
       </figure>
 
       <div className="flex w-full flex-col items-center">
-        <h2 className="mb-2.5 line-clamp-1 text-base font-bold text-black">
-          {title}
+        <h2 className="mb-2.5 line-clamp-1 w-full text-base font-bold text-black">
+          {reservation[0].lectureSchedule.lecture.title}
         </h2>
 
         <div className="grid w-full grid-cols-[3.3rem_1fr] gap-x-[0.69rem] gap-y-[0.44rem] text-sm">
-          <p>신청 횟수</p> <span>12회</span>
+          <p>신청 횟수</p> <span>{reservation.length}회</span>
           <p>수업 날짜</p>
           <div className="flex w-full flex-wrap gap-x-4 gap-y-1">
-            <span>23.10.02</span>
-            <span>23.10.02</span>
-            <span>23.10.02</span>
-            <span>23.10.02</span>
+            {[...uniqueClassDates].map((date) => (
+              <span key={date}>{date}</span>
+            ))}
           </div>
         </div>
       </div>
 
       <div className="col-span-2 mt-auto flex w-full items-center justify-between gap-3.5 whitespace-nowrap pr-2 text-sm md:col-span-1">
-        <div className="flex gap-2.5">
-          <button className="w-[4.69rem]">
-            <Link
-              href="/"
-              className={`h-7 font-semibold ${ButtonStyles.default}`}
-            >
-              커리큘럼
-            </Link>
+        <div className="flex flex-1 gap-2.5">
+          <button
+            onClick={handleCurriculumClick}
+            className={`h-7 w-[4.69rem] font-semibold ${ButtonStyles.default}`}
+          >
+            커리큘럼
           </button>
 
-          <div className="w-[4.69rem]">
+          <div className="w-full">
             <UniqueButton size="small" color="secondary">
-              {activeTab === 'progress' ? '수강취소' : '다시신청'}
+              {isProgress ? '수강취소' : '다시신청'}
             </UniqueButton>
           </div>
         </div>
 
         <ProfileImage
           size="small"
-          src={profile?.src || null}
-          nickname="가비쌤"
+          src={lecturer.lecturerProfileImageUrl[0].url}
+          nickname={lecturer.nickname}
         />
       </div>
     </li>
--- src/app/my/class/page.tsx
+++ src/app/my/class/page.tsx
@@ -1,28 +1,139 @@
 'use client';
-import { useState } from 'react';
-import ClassDetailView from './_components/ClassDetailView';
-import ClassListView from './_components/ClassListView';
+import { useState, useEffect } from 'react';
+import { getUserClass } from '@/lib/apis/classApis';
+import ClassList from './_components/ClassList';
+import Pagination from '@/components/Pagination/Pagination';
+import PageSizeSelector from '@/components/Selector/PageSizeSelector';
+import { IUserClassResponse } from '@/types/class';
 
-const MyClassPage = () => {
-  const [activeView, setActiveView] = useState('list');
+const ClassListView = () => {
+  const [isProgress, setIsProgress] = useState(true);
+  const [displayCount, setDisplayCount] = useState(5);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [itemsId, setItemsId] = useState({
+    firstItemId: 0,
+    lastItemId: 0,
+  });
+  const [classListData, setClassListData] = useState<IUserClassResponse[]>([]);
 
-  const handleItemClick = () => {
-    setActiveView('detail');
+  useEffect(() => {
+    fetchClassData(
+      displayCount,
+      0,
+      itemsId.firstItemId,
+      itemsId.lastItemId,
+      isProgress,
+    );
+  }, []);
+
+  const fetchClassData = async (
+    displayCount: number,
+    targetPage: number,
+    firstItemId: number,
+    lastItemId: number,
+    isProgress: boolean,
+  ) => {
+    const option = isProgress ? '진행중' : '수강 완료';
+    const classLists = await getUserClass(
+      displayCount,
+      currentPage,
+      targetPage,
+      firstItemId,
+      lastItemId,
+      option,
+    );
+
+    setClassListData(classLists);
+    setCurrentPage(targetPage);
+
+    setItemsId({
+      firstItemId: classLists[0].id,
+      lastItemId: classLists[displayCount - 1].id,
+    });
   };
 
-  const handleGoBack = () => {
-    setActiveView('list');
+  const handleActiveTab = () => {
+    setIsProgress((prev) => !prev);
+
+    fetchClassData(
+      displayCount,
+      currentPage,
+      itemsId.firstItemId,
+      itemsId.lastItemId,
+      !isProgress,
+    );
+
+    console.log(isProgress);
+  };
+  const handleDisplayCount = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newDisplayCount = Number(event.target.value);
+
+    setDisplayCount(newDisplayCount);
+
+    fetchClassData(
+      newDisplayCount,
+      currentPage,
+      itemsId.firstItemId,
+      itemsId.lastItemId,
+      isProgress,
+    );
+  };
+
+  const handlePageChange = async ({ selected }: { selected: number }) => {
+    setCurrentPage(selected);
+
+    fetchClassData(
+      displayCount,
+      selected,
+      itemsId.firstItemId,
+      itemsId.lastItemId,
+      isProgress,
+    );
   };
 
   return (
-    <>
-      {activeView === 'list' ? (
-        <ClassListView onItemClick={handleItemClick} />
-      ) : (
-        <ClassDetailView onGoBack={handleGoBack} />
-      )}
-    </>
+    <section className="col-span-2 mt-3.5 h-full w-full min-w-[22rem] max-w-[40rem] flex-col rounded-lg bg-white p-6 shadow-float">
+      <div className="flex gap-6 border-b border-solid pb-2 text-2xl text-gray-500">
+        <h1
+          onClick={handleActiveTab}
+          className={`${
+            isProgress
+              ? 'cursor-pointer font-bold text-black'
+              : 'cursor-pointer font-medium'
+          }`}
+        >
+          진행중/예정
+        </h1>
+        <h1
+          onClick={handleActiveTab}
+          className={`${
+            isProgress
+              ? 'cursor-pointer font-medium'
+              : 'cursor-pointer font-bold text-black'
+          }`}
+        >
+          수강완료
+        </h1>
+      </div>
+
+      <div className="mb-3.5 mt-2 flex w-full justify-end">
+        <PageSizeSelector value={displayCount} onChange={handleDisplayCount} />
+      </div>
+
+      <ul className="mb-9 flex flex-col gap-4">
+        {classListData.map((item) => (
+          <ClassList key={item.id} {...item} isProgress={isProgress} />
+        ))}
+      </ul>
+      <div className="w-full">
+        <Pagination
+          pageCount={5} // 추후 계산 필요
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </section>
   );
 };
 
-export default MyClassPage;
+export default ClassListView;
