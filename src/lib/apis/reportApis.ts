d59a055af943dc06d375a01130bc8b07380a7ea1
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-12-04 21:45:16 +0900

    feat(#170): 유저 신고내역 조회 API 로직 구현

--- src/lib/apis/reportApis.ts
+++ src/lib/apis/reportApis.ts
@@ -19,3 +19,30 @@ export const postReportLecturer = async (data: IReportRequest) => {
     }
   }
 };
+
+export const getUserReport = async (
+  displayCount: number,
+  currentPage: number,
+  targetPage: number,
+  firstItemId: number,
+  lastItemId: number,
+  type: string,
+) => {
+  const query = `take=${displayCount}&currentPage=${currentPage}&targetPage=${targetPage}&firstItemId=${firstItemId}&lastItemId=${lastItemId}&filterOption=${type}`;
+
+  try {
+    const response = await fetch(`${DOMAIN}/api/report/user?${query}`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((data) => data.json());
+
+    return response.data;
+  } catch (error) {
+    if (error instanceof Error && error.message) {
+      return error.message;
+    }
+  }
+};
