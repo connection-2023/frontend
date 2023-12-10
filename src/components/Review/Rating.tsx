29bae144425a8383362d2fc00215150f9865f5b0
hyeonjun-L <beeminor60@gmail.com>
2023-12-10 20:29:09 +0900

    refactor(#180): Rating 컴포넌트 리팩토링

--- src/components/Review/Rating.tsx
+++ src/components/Review/Rating.tsx
@@ -2,22 +2,44 @@
 import { useState, useEffect } from 'react';
 import { StarSVG } from '@/icons/svg';
 
-interface IRatingProps {
+interface ReadonlyProps {
   rate: number;
-  handleRate: (value: number) => void;
+  readonly: true;
+  reviewCount?: number;
+  viewRate?: boolean;
 }
 
-const Rating = ({ rate, handleRate }: IRatingProps) => {
+interface EditableProps {
+  rate: number;
+  handleRate: (value: number) => void;
+  reviewCount?: number;
+  readonly?: false;
+  viewRate?: boolean;
+}
+
+const Rating = (props: ReadonlyProps | EditableProps) => {
+  const {
+    rate,
+    readonly = false,
+    viewRate = true,
+    reviewCount = false,
+  } = props;
+  const handleRate = 'handleRate' in props ? props.handleRate : undefined;
+
   const [selectedStar, setSelectedStar] = useState(rate);
   const [hoveredStar, setHoveredStar] = useState(0);
 
   const handleClick = (index: number) => {
     if (selectedStar === index) {
       setSelectedStar(0);
-      handleRate(0);
+      if (handleRate) {
+        handleRate(0);
+      }
     } else {
       setSelectedStar(index);
-      handleRate(index);
+      if (handleRate) {
+        handleRate(index);
+      }
     }
   };
 
@@ -34,7 +56,7 @@ const Rating = ({ rate, handleRate }: IRatingProps) => {
   };
 
   return (
-    <div className="flex px-[0.94rem] py-[1.12rem]">
+    <div className="flex items-center px-[0.94rem] py-[1.12rem]">
       <div className="flex items-center">
         <div className="flex gap-1.5">
           {[...Array(5)].map((_, index) => (
@@ -42,21 +64,30 @@ const Rating = ({ rate, handleRate }: IRatingProps) => {
               key={index}
               width={18}
               height={17}
-              onClick={() => handleClick(index + 1)}
-              onMouseEnter={() => handleMouseEnter(index + 1)}
-              onMouseLeave={handleMouseLeave}
+              onClick={readonly ? undefined : () => handleClick(index + 1)}
+              onMouseEnter={
+                readonly ? undefined : () => handleMouseEnter(index + 1)
+              }
+              onMouseLeave={readonly ? undefined : handleMouseLeave}
               className={`${
                 Math.max(selectedStar, hoveredStar) >= index + 1
                   ? 'fill-sub-color1'
                   : 'fill-gray-700'
-              } cursor-pointer`}
+              } ${!readonly && 'cursor-pointer'}`}
             />
           ))}
         </div>
       </div>
-      <span className="ml-[0.94rem] h-5 w-6 text-sm font-bold">
-        {selectedStar > 0 ? selectedStar + '.0' : null}
-      </span>
+      {viewRate && (
+        <span className="ml-[0.94rem] h-5 w-6 text-sm font-bold">
+          {selectedStar > 0 ? selectedStar + '.0' : null}
+        </span>
+      )}
+      {reviewCount && (
+        <span className="ml-[0.94rem] text-lg font-semibold">
+          {reviewCount}개의 리뷰
+        </span>
+      )}
     </div>
   );
 };
