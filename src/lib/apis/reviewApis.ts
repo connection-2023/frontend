a6f414adec2767f7dee17f654daa768f2561c93c
hyeonjun-L <beeminor60@gmail.com>
2023-12-12 05:04:56 +0900

    feat(#180): 강사 마이 리뷰 client api 구현 및 연결

--- src/lib/apis/reviewApis.ts
+++ src/lib/apis/reviewApis.ts
@@ -1,5 +1,9 @@
 import { DOMAIN } from '@/constants/constants';
-import { WriteReview } from '@/types/review';
+import {
+  GetMyLecturersReviews,
+  GetMyLecturersReviewsData,
+  WriteReview,
+} from '@/types/review';
 import { FetchError } from '@/types/types';
 
 export const getWriteReviews = async (
@@ -25,6 +29,43 @@ export const getWriteReviews = async (
     return resData.data.review;
   } catch (error) {
     console.error('내 보유 강의 조회 오류', error);
+    throw error;
+  }
+};
+
+export const getMyLecturersReviews = async (
+  data: GetMyLecturersReviews,
+  signal?: AbortSignal,
+): Promise<GetMyLecturersReviewsData> => {
+  try {
+    const params = new URLSearchParams();
+
+    Object.entries(data)
+      .filter(([_, v]) => v !== undefined)
+      .forEach(([k, v]) => {
+        params.append(k, String(v));
+      });
+
+    const response = await fetch(`${DOMAIN}/api/review/lecturer?${params}`, {
+      method: 'GET',
+      credentials: 'include',
+      signal,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      const error: FetchError = new Error(errorData.message || '');
+      error.status = response.status;
+      throw error;
+    }
+
+    const resData = await response.json();
+    return resData.data.review;
+  } catch (error) {
+    console.error('강사 내 리뷰 불러오기', error);
     throw error;
   }
 };
