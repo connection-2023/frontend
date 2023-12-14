d62c13c1a3345a29d3e33627d02ac2e8d3ee8beb
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-12-14 17:08:03 +0900

    feat(#112): 강사 클래스 api 요청 로직 구현

---
+++ src/lib/apis/serverApis/instructorPostApis.ts
@@ -0,0 +1,20 @@
+import { IClassPostResponse } from '@/types/class';
+
+const END_POINT = process.env.NEXT_PUBLIC_API_END_POINT;
+
+export const getInstructorClassLists = async (
+  lecturerId: string,
+): Promise<IClassPostResponse[] | Error> => {
+  try {
+    const response = await fetch(
+      `${END_POINT}/lectures/lecturers/${lecturerId}`,
+      {
+        method: 'GET',
+      },
+    ).then((data) => data.json());
+
+    return response.data.lecture;
+  } catch (error) {
+    return new Error('잘못된 요청입니다!');
+  }
+};
