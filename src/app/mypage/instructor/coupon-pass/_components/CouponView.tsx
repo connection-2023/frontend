39b05cb0046ef6e711d6e0cc80a2b55f115c68dc
hyeonjun-L <beeminor60@gmail.com>
2023-12-18 11:39:38 +0900

    refactor(#188): 쿠폰 마이페이지 스타일 변경된 레이아웃 맞춰서 변경

--- src/app/mypage/instructor/coupon-pass/_components/CouponView.tsx
+++ src/app/mypage/instructor/coupon-pass/_components/CouponView.tsx
@@ -98,122 +98,124 @@ const CouponView = ({
   ];
 
   return (
-    <section className="z-0 col-start-2 flex w-full flex-col bg-white  pt-5 sm:px-5">
-      <nav className="flex justify-between pb-2">
-        <div className="flex items-center gap-2 sm:gap-6">
-          <button
-            className={`flex text-xl font-bold sm:text-2xl ${
-              filterState.isInterested === 'PASS' && 'text-gray-500'
-            }`}
-            onClick={() =>
-              router.push('/mypage/instructor/coupon-pass?state=coupon')
-            }
-          >
-            쿠폰({totalItemCount ?? 0})
-          </button>
-          <button
-            className={`text-xl font-bold sm:text-2xl ${
-              filterState.isInterested === 'COUPON' && 'text-gray-500'
-            }`}
-            onClick={() =>
-              router.push('/mypage/instructor/coupon-pass?state=pass')
-            }
-          >
-            패스권({passCount ?? 0})
-          </button>
-        </div>
-
-        <div className="w-[7.3rem]">
-          <Button>
-            <Link
-              href={{
-                pathname: '/mypage/instructor/coupon-pass/management',
-                query: { type: 'CREATE', state: 'coupon' },
-              }}
-              className="flex"
-            >
-              <CouponSVG className="mr-1 h-6 w-6 fill-sub-color1 group-active:fill-white" />
-              쿠폰 생성
-            </Link>
-          </Button>
-        </div>
-      </nav>
-
-      <nav className="flex flex-wrap items-center gap-2 border-y border-solid border-gray-500 py-5">
-        {options.map((option) => (
-          <button key={option.id} className="flex items-center gap-1">
-            <input
-              id={option.id}
-              type="checkbox"
-              className="peer h-[18px] w-[18px] accent-black"
-              checked={filterState.passStatusOptions === option.id}
-              onChange={() => handleChangeOptions(option.id)}
-            />
-            <label
-              htmlFor={option.id}
-              className="cursor-pointer text-gray-500 peer-checked:text-black"
-            >
-              {option.label}
-            </label>
-          </button>
-        ))}
-        <div className="w-80">
-          <ClassFilterSelect
-            options={myLectureList}
-            value={filterState.selectedClass}
-            onChange={handleChangeSelectedClass}
-          />
-        </div>
-      </nav>
-
-      <nav className="flex gap-2.5 py-4">
-        {filterState.passStatusOptions === 'AVAILABLE' &&
-          sortOptions.map((option) => (
+    <section className="z-0 flex w-full flex-col px-3 sm:px-6 md:px-9 xl:px-0">
+      <div className="z-0 flex w-full flex-col rounded-lg bg-white p-5 shadow-float">
+        <nav className="flex justify-between pb-2">
+          <div className="flex items-center gap-2 sm:gap-6">
             <button
-              key={option.id}
-              className={`flex text-sm font-bold ${
-                filterState.filterOption !== option.id && 'text-gray-500'
+              className={`flex text-xl font-bold sm:text-2xl ${
+                filterState.isInterested === 'PASS' && 'text-gray-500'
               }`}
-              onClick={() => handleFilterOptionChange(option.id)}
+              onClick={() =>
+                router.push('/mypage/instructor/coupon-pass?state=coupon')
+              }
             >
-              {option.label}
+              쿠폰({totalItemCount ?? 0})
+            </button>
+            <button
+              className={`text-xl font-bold sm:text-2xl ${
+                filterState.isInterested === 'COUPON' && 'text-gray-500'
+              }`}
+              onClick={() =>
+                router.push('/mypage/instructor/coupon-pass?state=pass')
+              }
+            >
+              패스권({passCount ?? 0})
+            </button>
+          </div>
+
+          <div className="w-[7.3rem]">
+            <Button>
+              <Link
+                href={{
+                  pathname: '/mypage/instructor/coupon-pass/management',
+                  query: { type: 'CREATE', state: 'coupon' },
+                }}
+                className="flex"
+              >
+                <CouponSVG className="mr-1 h-6 w-6 fill-sub-color1 group-active:fill-white" />
+                쿠폰 생성
+              </Link>
+            </Button>
+          </div>
+        </nav>
+
+        <nav className="flex flex-wrap items-center gap-2 border-y border-solid border-gray-500 py-5">
+          {options.map((option) => (
+            <button key={option.id} className="flex items-center gap-1">
+              <input
+                id={option.id}
+                type="checkbox"
+                className="peer h-[18px] w-[18px] accent-black"
+                checked={filterState.passStatusOptions === option.id}
+                onChange={() => handleChangeOptions(option.id)}
+              />
+              <label
+                htmlFor={option.id}
+                className="cursor-pointer text-gray-500 peer-checked:text-black"
+              >
+                {option.label}
+              </label>
             </button>
           ))}
-      </nav>
-
-      <div className="flex flex-wrap justify-center gap-4 pb-4 sm:justify-normal">
-        <CouponComponent
-          couponList={couponLists}
-          lastItemElementRef={lastItemElementRef}
-          totalItemCount={totalItemCount}
-          type="lecturer"
-          expiration={filterState.passStatusOptions}
-        />
-      </div>
-      {loading && width < 640 && (
-        <div className="mb-5 flex justify-center">
-          <Spinner />
-        </div>
-      )}
-
-      {!!totalItemCount ? (
-        <nav className="my-8 hidden sm:block">
-          <Pagination
-            pageCount={Math.ceil(totalItemCount / LECTURE_COUPON_TAKE)}
-            currentPage={
-              filterState.targetPage === 0 ? 0 : filterState.targetPage - 1
-            }
-            onPageChange={handleChangePage}
-          />
+          <div className="w-80">
+            <ClassFilterSelect
+              options={myLectureList}
+              value={filterState.selectedClass}
+              onChange={handleChangeSelectedClass}
+            />
+          </div>
         </nav>
-      ) : (
-        <div className="my-7 flex w-full flex-col items-center justify-center gap-8 text-lg font-semibold text-gray-100">
-          <NotFoundSVG />
-          <p>{`해당 ${
-            filterState.isInterested ? '쿠폰' : '패스권'
-          }이 없습니다!`}</p>
+
+        <nav className="flex gap-2.5 py-4">
+          {filterState.passStatusOptions === 'AVAILABLE' &&
+            sortOptions.map((option) => (
+              <button
+                key={option.id}
+                className={`flex text-sm font-bold ${
+                  filterState.filterOption !== option.id && 'text-gray-500'
+                }`}
+                onClick={() => handleFilterOptionChange(option.id)}
+              >
+                {option.label}
+              </button>
+            ))}
+        </nav>
+
+        <div className="flex flex-wrap justify-center gap-4 pb-4 sm:justify-normal">
+          <CouponComponent
+            couponList={couponLists}
+            lastItemElementRef={lastItemElementRef}
+            totalItemCount={totalItemCount}
+            type="lecturer"
+            expiration={filterState.passStatusOptions}
+          />
         </div>
-      )}
+        {loading && width < 640 && (
+          <div className="mb-5 flex justify-center">
+            <Spinner />
+          </div>
+        )}
+
+        {!!totalItemCount ? (
+          <nav className="my-8 hidden sm:block">
+            <Pagination
+              pageCount={Math.ceil(totalItemCount / LECTURE_COUPON_TAKE)}
+              currentPage={
+                filterState.targetPage === 0 ? 0 : filterState.targetPage - 1
+              }
+              onPageChange={handleChangePage}
+            />
+          </nav>
+        ) : (
+          <div className="my-7 flex w-full flex-col items-center justify-center gap-8 text-lg font-semibold text-gray-100">
+            <NotFoundSVG />
+            <p>{`해당 ${
+              filterState.isInterested ? '쿠폰' : '패스권'
+            }이 없습니다!`}</p>
+          </div>
+        )}
+      </div>
     </section>
   );
 };
