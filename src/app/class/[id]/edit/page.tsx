07e45d76ba4fd37bd9f1d0ba92c7e04e1ca3ed8b
hyeonjun-L <beeminor60@gmail.com>
2023-12-27 13:56:05 +0900

    feat(#190): 클래스 수정 공지사항, 운영계획, 커리큘럼 연결

--- src/app/class/[id]/edit/page.tsx
+++ src/app/class/[id]/edit/page.tsx
@@ -2,15 +2,12 @@
 import { parse, format, parseISO } from 'date-fns';
 import { usePathname } from 'next/navigation';
 import { useState, useEffect } from 'react';
-import { useForm, Controller } from 'react-hook-form';
-import EditClassRange from './_components/EditClassRange';
-import EditDayoff from './_components/EditDayoff';
-import SideNavbar from './_components/SideNavbar';
-import ScheduleView from '@/components/ScheduleView/ScheduleView';
-import CustomEditor from '@/components/TextArea/CustomEditor';
-import TextAreaSection from '@/components/TextArea/TextAreaSection';
-import UploadImage from '@/components/UploadImage/UploadImage';
-import { IClassEditData } from '@/types/class';
+import { useForm, Controller, FormProvider } from 'react-hook-form';
+import {
+  ANNOUNCEMENT,
+  CLASS_OPERATION_PLAN,
+  QUILL_DEFAULT_VALUE,
+} from '@/constants/constants';
 import {
   LocationSVG,
   TimeSVG,
@@ -24,6 +21,14 @@ import {
   formatLocationToString,
   formatGenreToString,
 } from '@/utils/parseUtils';
+import EditClassRange from './_components/EditClassRange';
+import EditDayoff from './_components/EditDayoff';
+import SideNavbar from './_components/SideNavbar';
+import ScheduleView from '@/components/ScheduleView/ScheduleView';
+import CustomEditor from '@/components/TextArea/CustomEditor';
+import TextAreaSection from '@/components/TextArea/TextAreaSection';
+import UploadImage from '@/components/UploadImage/UploadImage';
+import { IClassEditData } from '@/types/class';
 
 const borderStyle = 'border-b border-solid border-gray-700';
 const h2Style = 'mb-4 flex items-center text-lg font-bold';
@@ -33,7 +38,8 @@ const ClassEditPage = () => {
   const [initData, setInitData] = useState<IClassEditData | undefined>();
   const path = usePathname();
   const postId = path.split('/')[2];
-  const { control, watch, handleSubmit } = useForm();
+  const formMethods = useForm();
+  const { control, watch, handleSubmit } = formMethods;
   const watchClassRange = watch('classRange');
   const authUser = useUserStore((state) => state.authUser);
   const userType = useUserStore((state) => state.userType);
@@ -147,182 +153,215 @@ const ClassEditPage = () => {
       <SideNavbar onClick={handleSubmit(onSubmit)} />
 
       <div>
-        {/* 공지사항 */}
+        <FormProvider {...formMethods}>
+          <div className="flex flex-col gap-8 py-12">
+            {/* 공지사항 */}
+            <TextAreaSection
+              maxLength={200}
+              dataName="notification"
+              placeholder={ANNOUNCEMENT}
+              defaultValue={notification}
+              title="중요 공지사항을 입력해주세요"
+            />
 
-        {/* 운영계획 */}
+            {/* 운영계획 */}
+            <TextAreaSection
+              maxLength={500}
+              dataName="introduction"
+              defaultValue={introduction}
+              placeholder={CLASS_OPERATION_PLAN}
+              title="어떤 클래스를 운영할 계획 인가요?"
+              height="h-40"
+            />
 
-        {/* 커리큘럼 */}
-
-        {/* 클래스 일정 및 시간 */}
-        <section id="plan">
-          <Controller
-            name="classRange"
-            control={control}
-            defaultValue={dateRange}
-            rules={{
-              required: '전체 클래스 기간',
-              validate: ({ startDate, endDate }) => {
-                const dateFormat = /^\d{4}-\d{2}-\d{2}$/;
-                const isValidStartDate = dateFormat.test(startDate);
-                const isValidEndDate = dateFormat.test(endDate);
-
-                const parsedStartDate = parse(
-                  startDate,
-                  'yyyy-MM-dd',
-                  new Date(),
-                );
-                const parsedEndDate = parse(endDate, 'yyyy-MM-dd', new Date());
-
-                if (
-                  !isValidStartDate ||
-                  !isValidEndDate ||
-                  !format(parsedStartDate, 'yyyy-MM-dd') === startDate ||
-                  !format(parsedEndDate, 'yyyy-MM-dd') === endDate
-                ) {
-                  return '올바른 클래스 기간';
-                }
-
-                return true;
-              },
-            }}
-            render={({ field }) => (
-              <div className={`${borderStyle} py-6`}>
-                <h2 className={h2Style}>전체 클래스 기간을 설정해주세요</h2>
-                <div className="flex gap-x-4">
-                  <EditClassRange
-                    defaultValue={field.value}
-                    onChange={field.onChange}
-                  />
-                  <p className="flex items-center gap-x-2">
-                    <TimeSVG /> {duration}분
-                  </p>
-                </div>
-
-                {/* 추후 데이터 변경 필요 */}
-                <p className="mt-4 flex items-center gap-1.5 text-base">
-                  <span className="h-[7px] w-[7px] rounded-full bg-sub-color1" />
-                  월,수,금 15:00-16:00
-                </p>
-
-                {/* <div className="max-w-[37.4rem]">
-                  <ScheduleView
-                    maxCapacity={maxCapacity}
-                    duration={duration}
-                    lectureSchedule={formatSchedules()}
-                  />
-                </div> */}
-              </div>
-            )}
-          />
-
-          {/* 휴무일 */}
-          <Controller
-            name="holidays"
-            control={control}
-            defaultValue={{
-              schedules: initData?.schedule,
-              holidays: initData?.holidayArr,
-            }}
-            render={({ field }) => (
-              <div className={`${borderStyle} py-6`}>
-                <h2 className={h2Style}>휴무일이 있나요?</h2>
-
-                <EditDayoff
-                  onChange={field.onChange}
-                  defaultValue={field.value}
-                />
-              </div>
-            )}
-          />
-
-          {/* 신청 마감 시간 */}
-          <Controller
-            name="reservationDeadline"
-            control={control}
-            defaultValue={reservationDeadline}
-            rules={{
-              required: '신청 마감 시간',
-              min: { value: 1, message: '올바른 마감 시간' },
-            }}
-            render={({ field }) => (
-              <div className={`${borderStyle} py-6`}>
-                <h2 className={h2Style}>신청 마감 시간을 설정해주세요</h2>
-                <div className="ml-[0.38rem] text-sm font-medium text-gray-100">
-                  <span>수업 시작</span>
-                  <input
-                    type="number"
-                    defaultValue={field.value}
-                    onChange={(e) => {
-                      field.onChange(Number(e.target.value));
-                    }}
-                    className="ml-[1.38rem] mr-[0.38rem] h-8 w-12 rounded-md border border-solid border-gray-500 px-[0.81rem] py-1 focus:outline-sub-color1"
-                  />
-                  <span>시간 전</span>
-                </div>
-              </div>
-            )}
-          />
-
-          {/* 예약시 유의사항 */}
-          <Controller
-            name="reservationComment"
-            control={control}
-            defaultValue={reservationComment}
-            rules={{
-              required: '예약 시 유의사항',
-            }}
-            render={({ field }) => (
-              <div className={`${borderStyle} py-6`}>
-                <h2 className={h2Style}>예약시 유의사항</h2>
-                <textarea
-                  defaultValue={field.value}
-                  onChange={field.onChange}
-                  placeholder={`수강생이 클래스 신청시 예약 화면에서 보여지는 사항입니다. \n클래스를 시작하기 전 숙지해야 할 사항을 적어주세요`}
-                  className="h-32 w-full resize-none whitespace-pre-wrap break-keep rounded-md border border-solid border-gray-700 p-2"
-                />
-              </div>
-            )}
-          />
-        </section>
-        {/* 진행 장소 */}
-
-        {/* 가격 설정 */}
-        <section id="price" className="text-lg font-semibold">
-          <div className={`flex ${borderStyle} py-4`}>
-            <p className="mr-10 w-28">총 클래스 횟수</p>
-            <p>{initData.schedule.length}회</p>
+            {/* 커리큘럼 */}
+            <CustomEditor
+              title="커리큘럼"
+              dataName="curriculum"
+              defaultValue={curriculum}
+              placeholder={QUILL_DEFAULT_VALUE}
+              height="652px"
+              maxLength={3000}
+              minLength={200}
+              requiredMark={false}
+            />
           </div>
 
-          <div className={`flex ${borderStyle} py-4`}>
-            <p className="mr-10 w-28">1회 최대 수강생</p>
-            <div>명</div>
-          </div>
-
-          <div className="mb-10 flex py-4">
-            <p className="mr-10 w-28">가격 설정</p>
-
+          {/* 클래스 일정 및 시간 */}
+          <section id="plan">
             <Controller
-              name="price"
+              name="classRange"
               control={control}
-              defaultValue={price}
+              defaultValue={dateRange}
               rules={{
-                required: '가격',
+                required: '전체 클래스 기간',
+                validate: ({ startDate, endDate }) => {
+                  const dateFormat = /^\d{4}-\d{2}-\d{2}$/;
+                  const isValidStartDate = dateFormat.test(startDate);
+                  const isValidEndDate = dateFormat.test(endDate);
+
+                  const parsedStartDate = parse(
+                    startDate,
+                    'yyyy-MM-dd',
+                    new Date(),
+                  );
+                  const parsedEndDate = parse(
+                    endDate,
+                    'yyyy-MM-dd',
+                    new Date(),
+                  );
+
+                  if (
+                    !isValidStartDate ||
+                    !isValidEndDate ||
+                    !format(parsedStartDate, 'yyyy-MM-dd') === startDate ||
+                    !format(parsedEndDate, 'yyyy-MM-dd') === endDate
+                  ) {
+                    return '올바른 클래스 기간';
+                  }
+
+                  return true;
+                },
               }}
               render={({ field }) => (
-                <div className="ml-[0.38rem] text-gray-100">
-                  <span>1회당</span>
-                  <input
-                    type="number"
-                    className="ml-7 mr-1 h-8 w-24 rounded-md border border-solid border-gray-700 px-2 text-right focus:outline-sub-color1"
-                    defaultValue={field.value}
-                    onChange={field.onChange}
+                <div className={`${borderStyle} py-6`}>
+                  <h2 className={h2Style}>전체 클래스 기간을 설정해주세요</h2>
+                  <div className="flex gap-x-4">
+                    <EditClassRange
+                      defaultValue={field.value}
+                      onChange={field.onChange}
+                    />
+                    <p className="flex items-center gap-x-2">
+                      <TimeSVG /> {duration}분
+                    </p>
+                  </div>
+
+                  {/* 추후 데이터 변경 필요 */}
+                  <p className="mt-4 flex items-center gap-1.5 text-base">
+                    <span className="h-[7px] w-[7px] rounded-full bg-sub-color1" />
+                    월,수,금 15:00-16:00
+                  </p>
+
+                  {/* <div className="max-w-[37.4rem]">
+                  <ScheduleView
+                  maxCapacity={maxCapacity}
+                  duration={duration}
+                  lectureSchedule={formatSchedules()}
                   />
-                  <span>원</span>
+                </div> */}
                 </div>
               )}
             />
-          </div>
-        </section>
+
+            {/* 휴무일 */}
+            <Controller
+              name="holidays"
+              control={control}
+              defaultValue={{
+                schedules: initData?.schedule,
+                holidays: initData?.holidayArr,
+              }}
+              render={({ field }) => (
+                <div className={`${borderStyle} py-6`}>
+                  <h2 className={h2Style}>휴무일이 있나요?</h2>
+
+                  <EditDayoff
+                    onChange={field.onChange}
+                    defaultValue={field.value}
+                  />
+                </div>
+              )}
+            />
+
+            {/* 신청 마감 시간 */}
+            <Controller
+              name="reservationDeadline"
+              control={control}
+              defaultValue={reservationDeadline}
+              rules={{
+                required: '신청 마감 시간',
+                min: { value: 1, message: '올바른 마감 시간' },
+              }}
+              render={({ field }) => (
+                <div className={`${borderStyle} py-6`}>
+                  <h2 className={h2Style}>신청 마감 시간을 설정해주세요</h2>
+                  <div className="ml-[0.38rem] text-sm font-medium text-gray-100">
+                    <span>수업 시작</span>
+                    <input
+                      type="number"
+                      defaultValue={field.value}
+                      onChange={(e) => {
+                        field.onChange(Number(e.target.value));
+                      }}
+                      className="ml-[1.38rem] mr-[0.38rem] h-8 w-12 rounded-md border border-solid border-gray-500 px-[0.81rem] py-1 focus:outline-sub-color1"
+                    />
+                    <span>시간 전</span>
+                  </div>
+                </div>
+              )}
+            />
+
+            {/* 예약시 유의사항 */}
+            <Controller
+              name="reservationComment"
+              control={control}
+              defaultValue={reservationComment}
+              rules={{
+                required: '예약 시 유의사항',
+              }}
+              render={({ field }) => (
+                <div className={`${borderStyle} py-6`}>
+                  <h2 className={h2Style}>예약시 유의사항</h2>
+                  <textarea
+                    defaultValue={field.value}
+                    onChange={field.onChange}
+                    placeholder={`수강생이 클래스 신청시 예약 화면에서 보여지는 사항입니다. \n클래스를 시작하기 전 숙지해야 할 사항을 적어주세요`}
+                    className="h-32 w-full resize-none whitespace-pre-wrap break-keep rounded-md border border-solid border-gray-700 p-2"
+                  />
+                </div>
+              )}
+            />
+          </section>
+          {/* 진행 장소 */}
+
+          {/* 가격 설정 */}
+          <section id="price" className="text-lg font-semibold">
+            <div className={`flex ${borderStyle} py-4`}>
+              <p className="mr-10 w-28">총 클래스 횟수</p>
+              <p>{initData.schedule.length}회</p>
+            </div>
+
+            <div className={`flex ${borderStyle} py-4`}>
+              <p className="mr-10 w-28">1회 최대 수강생</p>
+              <div>명</div>
+            </div>
+
+            <div className="mb-10 flex py-4">
+              <p className="mr-10 w-28">가격 설정</p>
+
+              <Controller
+                name="price"
+                control={control}
+                defaultValue={price}
+                rules={{
+                  required: '가격',
+                }}
+                render={({ field }) => (
+                  <div className="ml-[0.38rem] text-gray-100">
+                    <span>1회당</span>
+                    <input
+                      type="number"
+                      className="ml-7 mr-1 h-8 w-24 rounded-md border border-solid border-gray-700 px-2 text-right focus:outline-sub-color1"
+                      defaultValue={field.value}
+                      onChange={field.onChange}
+                    />
+                    <span>원</span>
+                  </div>
+                )}
+              />
+            </div>
+          </section>
+        </FormProvider>
       </div>
     </main>
   );
