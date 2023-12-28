3bd9170597a289d38dea42013b3d1eb812e161ed
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-12-29 05:07:42 +0900

    feat(#76): INITIAL_SCHEDULE_PROGRESS 추가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -289,3 +289,21 @@ export const LECTURE_COUPON_TAKE = 8;
 export const LECTURE_PASS_TAKE = 8;
 
 export const USER_COUPON_CLASS_LIST_TAKE = 6;
+
+export const INITIAL_SCHEDULE_PROGRESS = [
+  {
+    text: '수업 완료',
+    count: 0,
+    color: 'text-main-color',
+  },
+  {
+    text: '수업 예정',
+    count: 0,
+    color: 'text-sub-color1',
+  },
+  {
+    text: '총 수업',
+    count: 0,
+    color: '',
+  },
+];
