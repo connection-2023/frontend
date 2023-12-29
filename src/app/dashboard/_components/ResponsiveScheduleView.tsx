ca83fd2f0da284eb5026f70befdc600c3a6020a9
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-12-30 02:19:22 +0900

    feat(#76): 클래스 일정 api 데이터 가공 및 연결

--- src/app/dashboard/_components/ResponsiveScheduleView.tsx
+++ src/app/dashboard/_components/ResponsiveScheduleView.tsx
@@ -1,55 +1,63 @@
+import { format, parseISO } from 'date-fns';
+import { nanoid } from 'nanoid';
 import Link from 'next/link';
+import { useMemo } from 'react';
 import { ClassStatusSVG } from '@/icons/svg';
+import { IMonthlyClassSchedules } from '@/types/class';
 
-const ResponsiveScheduleView = () => {
-  return (
-    <ul className="flex flex-col gap-4">
-      <ScheduleList
-        status="private"
-        time="09:00-09:50"
-        title="리아킴과 함께하는 댄스클래스"
-      />
-      <ScheduleList
-        status="group"
-        time="12:00-12:50"
-        title="리아킴과 함께하는 댄스클래스"
-      />
-      <ScheduleList
-        status="full"
-        time="18:00-18:50"
-        title="리아킴과 함께하는 댄스클래스"
-      />
-    </ul>
-  );
+const statusColor: Record<string, string> = {
+  private: 'fill-sub-color1',
+  group: 'fill-main-color',
+  full: 'fill-sub-color2',
 };
 
-interface ScheduleListProps {
-  status: 'private' | 'group' | 'full';
-  time: string;
-  title: string;
-}
-// API 연결하면서 위 컴포넌트 안으로 이동 예정
-const ScheduleList = ({ status, time, title }: ScheduleListProps) => {
-  const statusColor = {
-    private: 'fill-sub-color1',
-    group: 'fill-main-color',
-    full: 'fill-sub-color2',
-  };
+const ResponsiveScheduleView = ({
+  selectedEvent,
+}: {
+  selectedEvent: IMonthlyClassSchedules[];
+}) => {
+  const schedules = useMemo(
+    () =>
+      selectedEvent.map((event) => {
+        const status = event.lecture.isGroup
+          ? 'group'
+          : event.numberOfParticipants === event.lecture.maxCapacity
+          ? 'full'
+          : 'private';
+        const formattedStart = format(parseISO(event.startDateTime), 'HH:mm');
+        const formattedEnd = format(parseISO(event.endDateTime), 'HH:mm');
+
+        return {
+          status,
+          title: event.lecture.title,
+          time: `${formattedStart}-${formattedEnd}`,
+          numberOfParticipants: event.numberOfParticipants,
+          maxCapacity: event.lecture.maxCapacity,
+          lectureId: event.lectureId,
+        };
+      }),
+    [selectedEvent],
+  );
 
   return (
-    <li className="flex flex-col text-sm">
-      <p className="mb-2 flex items-center gap-1 whitespace-nowrap font-semibold">
-        <ClassStatusSVG
-          width="11"
-          height="11"
-          className={statusColor[status]}
-        />
-        {time}
-      </p>
-      <Link href="/id" className="font-medium">
-        {title}
-      </Link>
-    </li>
+    <ul className="flex flex-col gap-2">
+      {schedules.map((schedule) => (
+        <li key={nanoid()} className="flex text-sm">
+          <p className="mb-2 flex w-28 items-center gap-1 whitespace-nowrap font-semibold">
+            <ClassStatusSVG
+              width="11"
+              height="11"
+              className={statusColor[schedule.status]}
+            />
+            {schedule.time}
+          </p>
+          <Link href={`/class/${schedule.lectureId}`} className="font-medium">
+            {schedule.title}
+            <span className="text-gray-300">{` (${schedule.numberOfParticipants}/${schedule.maxCapacity})`}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
