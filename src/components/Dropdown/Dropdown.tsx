5e137083b2da2b5d23b2d4e1c75b056bc7ab46ef
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-01-08 23:50:04 +0900

    feat(#213): className props 추가

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,19 +1,21 @@
 'use client';
-
 interface DropdownProps {
   options: {
-    onClick: React.MouseEventHandler<HTMLLIElement>;
+    onClick?: React.MouseEventHandler<HTMLLIElement>;
     component: React.ReactNode;
   }[];
+  className?: string;
 }
 
-const Dropdown = ({ options }: DropdownProps) => (
-  <ul className="absolute right-0 top-8 flex flex-col divide-y divide-solid divide-gray-700 overflow-hidden rounded-md border border-solid border-black bg-white text-sm font-medium text-gray-300">
+const Dropdown = ({ options, className }: DropdownProps) => (
+  <ul
+    className={`${className} absolute z-modal flex flex-col divide-y divide-solid divide-gray-700 overflow-hidden rounded-md border border-solid border-black bg-white bg-white text-sm font-medium text-gray-300`}
+  >
     {options.map((list, index) => (
       <li
         key={index}
         className="flex h-8 cursor-pointer items-center gap-2 px-3 text-black hover:bg-gray-900"
-        onClick={list.onClick}
+        onClick={list.onClick || undefined}
       >
         {list.component}
       </li>
