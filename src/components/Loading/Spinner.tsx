c06fb866cce3bc7b6ecfd56f2f7ce52b63997e15
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-01-17 01:46:47 +0900

    feat: spinner 스타일 변경

--- src/components/Loading/Spinner.tsx
+++ src/components/Loading/Spinner.tsx
@@ -1,29 +1,9 @@
-'use client';
-import { motion } from 'framer-motion';
-import React, { useEffect, useState } from 'react';
 import { SpinnerSVG } from '@/icons/svg';
 
-const Spinner = ({ size }: { size: number }) => {
-  const sizeStyle = `h-[${size}] w-[${size}]`;
-  const [rotation, setRotation] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setRotation((prev) => (prev + 45) % 360);
-    }, 200);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <motion.div
-      className={`${sizeStyle} overflow-hidden rounded-full`}
-      style={{ rotate: rotation }}
-      animate={{ rotate: rotation }}
-    >
-      <SpinnerSVG width={size} height={size} />
-    </motion.div>
-  );
-};
+const Spinner = ({ size = 80 }: { size?: number }) => (
+  <div className="spinner">
+    <SpinnerSVG width={size} height={size} />
+  </div>
+);
 
 export default Spinner;
