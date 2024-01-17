379296c90958e42af65ac233b4a6ada459871270
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-01-18 01:26:21 +0900

    feat(#221): 승인 리스트 API 연결

--- src/app/mypage/instructor/manage/approval-pending/_components/ClassList.tsx
+++ src/app/mypage/instructor/manage/approval-pending/_components/ClassList.tsx
@@ -1,16 +1,24 @@
 import PendingList from './PendingList';
 import Button from '@/components/Button/Button';
+import { IApproveList } from '@/types/instructor';
 
-const ClassList = () => {
-  const handleAllApprove = () => {};
+const ClassList = ({ classItem }: { classItem: IApproveList }) => {
+  const { lecture, payments } = classItem;
+
+  const handleAllApprove = () => {
+    // 추후 API 개발 후 연동 예정
+  };
 
   return (
     <section className="flex flex-col border-b border-solid border-gray-500 pb-6">
       <div className="flex items-baseline justify-between gap-x-2.5 px-3.5 md:p-0">
         <div className="gap-x-2.5 md:flex md:p-0">
-          <h2 className="mb-1.5 font-bold">클래스 제목 </h2>
+          <h2 className="mb-1.5 font-bold">{lecture.title}</h2>
           <p className="flex-1 text-sub-color1 md:text-black">
-            노쇼 위약금 <span className="font-bold">10,000원</span>
+            노쇼 위약금
+            <span className="ml-1 font-bold">
+              {lecture.noShowDeposit.toLocaleString()}원
+            </span>
           </p>
         </div>
         <div className="w-[4.5rem] text-sm">
@@ -21,10 +29,14 @@ const ClassList = () => {
       </div>
 
       <ul className="mt-4 flex w-full flex-col gap-y-2 divide-y divide-gray-700 text-base text-gray-100 md:divide-none">
-        <PendingList />
-        <PendingList />
-        <PendingList />
-        <PendingList />
+        {payments.map((payment) => (
+          <PendingList
+            key={payment.id}
+            payment={payment}
+            lectureId={lecture.id}
+            title={lecture.title}
+          />
+        ))}
       </ul>
     </section>
   );
