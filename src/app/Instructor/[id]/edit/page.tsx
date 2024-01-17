753ae453c1de0f6f87d71d792dc477702ff27c0f
hyeonjun-L <beeminor60@gmail.com>
2024-01-18 03:52:23 +0900

    fix/feat(#222): 강사 유저 전환 시 서버 userStore state 변환 안되는 버그 해결 및 강사 수정 페이지 라우터 보호 구현

--- src/app/Instructor/[id]/edit/page.tsx
+++ src/app/Instructor/[id]/edit/page.tsx
@@ -1,4 +1,18 @@
-const page = () => {
+import { redirect } from 'next/navigation';
+import { getInstructor } from '@/lib/apis/serverApis/instructorPostApis';
+import { useUserStore } from '@/store';
+
+const page = async ({ params: { id } }: { params: { id: string } }) => {
+  const { authUser } = useUserStore.getState();
+
+  if (Number(authUser?.id) !== Number(id)) {
+    redirect('/404');
+  }
+
+  try {
+    await getInstructor(id, false);
+  } catch (error) {}
+
   return <section>aaa</section>;
 };
 
