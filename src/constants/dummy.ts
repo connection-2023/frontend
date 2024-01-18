67c8fd2aafc6f1eb24e7f3692e16f7d2e7f4d544
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-01-19 03:12:31 +0900

    chore(#208): 불필요한 데이터 삭제

--- src/constants/dummy.ts
+++ src/constants/dummy.ts
@@ -1,4 +1,3 @@
-import { ClassCardType } from '@/types/class';
 import { INotice, IPaymentList } from '@/types/types';
 
 const dummyUserInputSuggestion = [
@@ -124,206 +123,16 @@ const dummyClass = {
   },
 };
 
-const dummyImgURL = [
-  'https://img.freepik.com/free-photo/pretty-woman-practising-hip-hop-dance_107420-85008.jpg?size=626&ext=jpg',
-  'https://img.freepik.com/free-photo/girl-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9249.jpg?size=626&ext=jpg',
-  'https://img.freepik.com/free-photo/dance-time-stylish-men-and-woman-dancing-hip-hop-in-bright-clothes-on-green-background-at-dance-hall-in-neon-light_155003-16406.jpg?size=626&ext=jpg',
-  'https://img.freepik.com/free-photo/two-beautiful-slender-girls-doing-dancing-and-gymnastics-in-the-dance-hall_1157-13817.jpg?size=626&ext=jpg',
-  'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-];
-
-const dummyInstructor = {
-  imgURL: dummyImgURL,
-  nickname: '리아킴',
-  location: '서울시 마포구',
-  isLiked: false,
-  genre: ['힙합', '스트릿댄스', '현대무용'],
-  teamAffiliation: ['원밀리언', '프리랜서'],
-  instagramID: '@Connection',
-  youtubeURL: 'https://www.youtube.com/@Connection_dance',
-  anyURL: 'https://www.Connection.com',
-  instagrams: [
-    'https://www.instagram.com/reel/CqDcjXEJQt_',
-    'https://www.instagram.com/p/CtagBRcPETS',
-    'https://www.instagram.com/p/CtvYoispwLg',
-  ],
-  classList: [
-    {
-      id: 1,
-      status: '모집중' as const,
-      date: '08/04~09/25',
-      title: '가비쌤과 함께하는 왁킹 클래스',
-      location: ['서울 마포구'],
-      genre: ['락킹', '락킹'],
-      type: '단체레슨',
-      time: ['오전', '오후'],
-      review: { average: 4.5, count: 14 },
-      price: 80000,
-      profile: { src: null, nickname: 'nickname' },
-      isLiked: false,
-      selectedDates: [
-        new Date(2023, 8, 4),
-        new Date(2023, 8, 6),
-        new Date(2023, 8, 8),
-      ],
-      imgURL: [
-        'https://img.freepik.com/free-photo/pretty-woman-practising-hip-hop-dance_107420-85008.jpg?size=626&ext=jpg',
-        'https://img.freepik.com/free-photo/girl-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9249.jpg?size=626&ext=jpg',
-        'https://img.freepik.com/free-photo/dance-time-stylish-men-and-woman-dancing-hip-hop-in-bright-clothes-on-green-background-at-dance-hall-in-neon-light_155003-16406.jpg?size=626&ext=jpg',
-        'https://img.freepik.com/free-photo/two-beautiful-slender-girls-doing-dancing-and-gymnastics-in-the-dance-hall_1157-13817.jpg?size=626&ext=jpg',
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-      ],
-    },
-    {
-      id: 2,
-      status: '모집중' as const,
-      date: '08/04~09/25',
-      title: '가비쌤과 함께하는 왁킹 클래스',
-      location: ['서울 마포구', '서울 동작구'],
-      genre: ['락킹', '락킹'],
-      type: '개인레슨',
-      time: ['오전'],
-      isLiked: false,
-      review: { average: 4.5, count: 14 },
-      price: 80000,
-      profile: { src: null, nickname: 'nickname' },
-      selectedDates: [new Date()],
-      imgURL: [
-        'https://img.freepik.com/free-photo/pretty-woman-practising-hip-hop-dance_107420-85008.jpg?size=626&ext=jpg',
-        'https://img.freepik.com/free-photo/girl-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9249.jpg?size=626&ext=jpg',
-        'https://img.freepik.com/free-photo/dance-time-stylish-men-and-woman-dancing-hip-hop-in-bright-clothes-on-green-background-at-dance-hall-in-neon-light_155003-16406.jpg?size=626&ext=jpg',
-        'https://img.freepik.com/free-photo/two-beautiful-slender-girls-doing-dancing-and-gymnastics-in-the-dance-hall_1157-13817.jpg?size=626&ext=jpg',
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-      ],
-    },
-    {
-      id: 3,
-      status: '마감' as const,
-      date: '08/04~09/25',
-      title: '가비쌤과 함께하는 왁킹 클래스',
-      location: ['서울 마포구', '서울 성동구'],
-      genre: ['락킹'],
-      type: '개인레슨',
-      time: ['오전'],
-      isLiked: false,
-      review: { average: 4, count: 12 },
-      price: 80000,
-      profile: { src: null, nickname: 'nickname' },
-      selectedDates: [new Date()],
-      imgURL: [
-        'https://img.freepik.com/free-photo/pretty-woman-practising-hip-hop-dance_107420-85008.jpg?size=626&ext=jpg',
-        'https://img.freepik.com/free-photo/girl-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9249.jpg?size=626&ext=jpg',
-        'https://img.freepik.com/free-photo/dance-time-stylish-men-and-woman-dancing-hip-hop-in-bright-clothes-on-green-background-at-dance-hall-in-neon-light_155003-16406.jpg?size=626&ext=jpg',
-        'https://img.freepik.com/free-photo/two-beautiful-slender-girls-doing-dancing-and-gymnastics-in-the-dance-hall_1157-13817.jpg?size=626&ext=jpg',
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-      ],
-    },
-  ],
-  review: {
-    average: 4.17,
-    count: '2,324',
-    reviewer: [
-      {
-        nickname: 'nickname',
-        average: 4,
-        content: '후기 350자 제한, 답글은 안다는걸로, 사진은 없는걸로',
-        src: null,
-        date: '2023.07.11',
-        title: '가비쌤과 함께하는 왁킹 클래스',
-      },
-      {
-        nickname: 'nickname1',
-        average: 4,
-        content: '후기 350자 제한, 답글은 안다는걸로, 사진은 없는걸로',
-        src: null,
-        date: '2023.07.11',
-        title: '가비쌤과 함께하는 왁킹 클래스',
-      },
-      {
-        nickname: 'nickname2',
-        average: 2,
-        content: '후기 350자 제한, 답글은 안다는걸로, 사진은 없는걸로',
-        src: null,
-        date: '2023.07.11',
-        title: '가비쌤과 함께하는 왁킹 클래스',
-      },
-      {
-        nickname: 'nickname3',
-        average: 3,
-        content: '후기 350자 제한, 답글은 안다는걸로, 사진은 없는걸로',
-        src: null,
-        date: '2023.07.11',
-        title: '가비쌤과 함께하는 왁킹 클래스',
-      },
-    ],
-  },
-};
-
 interface MainProps {
-  topInstructorList: {
-    nickname: string;
-    id: number;
-    image: string;
-  }[];
   suggestionClass: {
     title: string;
     image: string;
     range: string;
     review: string;
   }[];
-  recentClass: ClassCardType[];
 }
+
 const dummyMain: MainProps = {
-  topInstructorList: [
-    {
-      nickname: 'Harriet',
-      id: 7,
-      image:
-        'https://img.freepik.com/free-photo/pretty-woman-practising-hip-hop-dance_107420-85008.jpg?size=626&ext=jpg',
-    },
-    {
-      nickname: 'Wang',
-      id: 4,
-      image:
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-    },
-    {
-      nickname: 'Ignacia',
-      id: 8,
-      image:
-        'https://img.freepik.com/free-photo/two-beautiful-slender-girls-doing-dancing-and-gymnastics-in-the-dance-hall_1157-13817.jpg?size=626&ext=jpg',
-    },
-    {
-      nickname: 'Lana',
-      id: 10,
-      image:
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-    },
-    {
-      nickname: 'Edward',
-      id: 4,
-      image:
-        'https://img.freepik.com/free-photo/girl-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9249.jpg?size=626&ext=jpg',
-    },
-    {
-      nickname: 'Walter',
-      id: 9,
-      image:
-        'https://img.freepik.com/free-photo/pretty-woman-practising-hip-hop-dance_107420-85008.jpg?size=626&ext=jpg',
-    },
-    {
-      nickname: 'Marah',
-      id: 10,
-      image:
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-    },
-    {
-      nickname: 'Alden',
-      id: 8,
-      image:
-        'https://img.freepik.com/free-photo/pretty-woman-practising-hip-hop-dance_107420-85008.jpg?size=626&ext=jpg',
-    },
-  ],
   suggestionClass: [
     {
       title: '21년 최고매출, 22년 최다판매 친절한 쁨선생 케이팝',
@@ -354,174 +163,6 @@ const dummyMain: MainProps = {
       review: '2.6',
     },
   ],
-  recentClass: [
-    {
-      id: 3,
-      status: '모집중',
-      date: '2023-10-01',
-      title: '요리 클래스',
-      isLiked: false,
-      imgURL: [
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-      ],
-      location: ['서울', '강남구'],
-      genre: ['요리'],
-      type: '오프라인',
-      review: { average: 4.5, count: 20 },
-      price: 50000,
-      profile: {
-        src: 'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-        nickname: '요리사',
-      },
-      // selectedDates: [new Date('2023-10-01'), new Date('2023-10-02')],
-    },
-    {
-      id: 4,
-      status: '마감',
-      date: '2023-10-03',
-      title: '피아노 클래스',
-      isLiked: false,
-      imgURL: [
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-      ],
-      location: ['서울', '종로구'],
-      genre: ['음악', '피아노'],
-      type: '오프라인',
-      //time: ['18시~20시'],
-      review: { average: 4.7, count: 25 },
-      price: 70000,
-      profile: {
-        src: 'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-        nickname: '피아니스트',
-      },
-      // selectedDates: [new Date('2023-10-03'), new Date('2023-10-04')],
-    },
-    {
-      id: 5,
-      status: '마감',
-      date: '2023-10-05',
-      title: '미술 클래스 ',
-      isLiked: true,
-      imgURL: [
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-      ],
-      location: ['부산', '해운대구'],
-      genre: ['미술'],
-      type: '오프라인',
-      //time: ['15시~17시'],
-      review: { average: 4.9, count: 30 },
-      price: 60000,
-      profile: {
-        src: 'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-        nickname: '화가',
-      },
-      //selectedDates: [new Date('2023_10_05'), new Date('2023_10_06')],
-    },
-    {
-      id: 6,
-      status: '모집중',
-      date: '2023_11_07',
-      isLiked: true,
-      title: '사진 촬영 클래스 ',
-      imgURL: [
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-      ],
-      location: ['대전', '유성구'],
-      genre: ['사진'],
-      type: '오프라인',
-      //  time: ['13시~15시'],
-      review: { average: 4.6, count: 22 },
-      price: 80000,
-      profile: {
-        src: 'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-        nickname: '사진작가',
-      },
-      // selectedDates: [new Date('2023_11_07'), new Date('2023_11_08')],
-    },
-    {
-      id: 6,
-      status: '마감',
-      date: '2023-12-10',
-      title: '코딩 클래스 ',
-      isLiked: true,
-      imgURL: [
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-      ],
-      location: ['인천', '연수구'],
-      genre: ['코딩'],
-      type: '온라인',
-      // time: ['20시~22시'],
-      review: { average: 4.8, count: 27 },
-      price: 90000,
-      profile: {
-        src: 'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-        nickname: '개발자',
-      },
-      //  selectedDates: [new Date('2023-12-10'), new Date('2023-12-11')],
-    },
-    {
-      id: 7,
-      status: '모집중',
-      date: '2024-01-15',
-      isLiked: true,
-      title: '영어회화 클래스',
-      imgURL: [
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-      ],
-      location: ['광주', '서구'],
-      genre: ['언어', '영어'],
-      type: '오프라인',
-      // time: ['16시~18시'],
-      review: { average: 5.0, count: 32 },
-      price: 100000,
-      profile: {
-        src: 'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-        nickname: '영어강사',
-      },
-      // selectedDates: [new Date('2024-01-15'), new Date('2024-01-16')],
-    },
-    {
-      id: 9,
-      status: '모집중',
-      date: '2024_02_20',
-      isLiked: true,
-      title: '체육 클래스',
-      imgURL: [
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-      ],
-      location: ['울산', '남구'],
-      genre: ['스포츠', '체육'],
-      type: '오프라인',
-      //time: ['09시~11시'],
-      review: { average: 4.9, count: 33 },
-      price: 110000,
-      profile: {
-        src: 'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-        nickname: '운동선수',
-      },
-      //selectedDates: [new Date('2024_02_20'), new Date('2024_02_21')],
-    },
-    {
-      id: 10,
-      status: '모집중',
-      date: '2024_03_25',
-      isLiked: true,
-      title: '과학 실험 클래스 ',
-      imgURL: [
-        'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-      ],
-      location: ['대구', '중구'],
-      genre: ['과학'],
-      type: '오프라인',
-      review: { average: 5.0, count: 35 },
-      price: 120000,
-      profile: {
-        src: 'https://img.freepik.com/free-photo/boy-dancing-hip-hop-in-stylish-clothes-on-gradient-background-at-dance-hall-in-neon-light_155003-9262.jpg?size=626&ext=jpg',
-        nickname: '과학자',
-      },
-      //selectedDates: [new Date('2024_03_25'), new Date('2024_03_26')],
-    },
-  ],
 };
 
 const dummyUserInfo = {
@@ -531,72 +172,6 @@ const dummyUserInfo = {
   alarmCount: 9,
   commentCount: 10,
 };
-
-const dummyClasstableData = [
-  {
-    classInfo: '1회차',
-    dateTime: '23.09.19 13:00-15:00',
-    apply: {
-      total: 6,
-      current: 5,
-    },
-    duedate: '23.09.18 14:00',
-  },
-  {
-    classInfo: '2회차',
-    dateTime: '23.9.19 13:00-15:00',
-    apply: {
-      total: 6,
-      current: 5,
-    },
-    duedate: '23.09.18 14:00',
-  },
-  {
-    classInfo: '3회차',
-    dateTime: '23.09.29 13:00-15:00',
-    apply: {
-      total: 6,
-      current: 5,
-    },
-    duedate: '23.09.18 14:00',
-  },
-  {
-    classInfo: '4회차',
-    dateTime: '23.10.01 13:00-15:00',
-    apply: {
-      total: 6,
-      current: 5,
-    },
-    duedate: '23.09.18 14:00',
-  },
-  {
-    classInfo: '5회차',
-    dateTime: '23.10.03 13:00-15:00',
-    apply: {
-      total: 6,
-      current: 5,
-    },
-    duedate: '23.10.01 14:00',
-  },
-  {
-    classInfo: '6회차',
-    dateTime: '23.10.18 13:00-15:00',
-    apply: {
-      total: 6,
-      current: 5,
-    },
-    duedate: '23.10.18 14:00',
-  },
-  {
-    classInfo: '7회차',
-    dateTime: '23.10.22 13:00-15:00',
-    apply: {
-      total: 6,
-      current: 5,
-    },
-    duedate: '23.10.18 14:00',
-  },
-];
 
 const dummyEnrollmentPerson = [
   {
@@ -612,96 +187,6 @@ const dummyEnrollmentPerson = [
     nickname: '닉네임2',
     memo: '',
     request: null,
-  },
-];
-
-const dummyClassRoster = [
-  {
-    id: 1,
-    src: null,
-    nickname: '제이제이',
-    applyCount: 3,
-  },
-  {
-    id: 2,
-    src: null,
-    nickname: '구슬',
-    applyCount: 1,
-  },
-  {
-    id: 3,
-    src: null,
-    nickname: '제이제이',
-    applyCount: 3,
-  },
-  {
-    id: 4,
-    src: null,
-    nickname: '구슬',
-    applyCount: 1,
-  },
-  {
-    id: 5,
-    src: null,
-    nickname: '제이제이제이제이',
-    applyCount: 3,
-  },
-  {
-    id: 6,
-    src: null,
-    nickname: '구슬',
-    applyCount: 1,
-  },
-  {
-    id: 7,
-    src: null,
-    nickname: '제이제이',
-    applyCount: 3,
-  },
-  {
-    id: 8,
-    src: null,
-    nickname: '구슬',
-    applyCount: 1,
-  },
-  {
-    id: 9,
-    src: null,
-    nickname: '구슬',
-    applyCount: 1,
-  },
-];
-
-const dummyClassList = [
-  {
-    id: 1,
-    status: '모집중',
-    date: '23.08.03-23.09.28',
-    title: '"원밀리언 댄스 스튜디오 with리아킴" 에게 배우는 댄스 입문',
-    count: {
-      current: 16,
-      total: 20,
-    },
-  },
-  {
-    id: 2,
-    status: '모집중',
-    date: '23.08.03-23.09.28',
-    title: '"원밀리언 댄스 스튜디오 with리아킴" 에게 배우는 댄스 입문',
-    count: {
-      current: 16,
-      total: 20,
-    },
-  },
-  {
-    id: 3,
-    status: '모집중',
-    date: '23.08.03-23.09.28',
-    title: '"원밀리언 댄스 스튜디오 with리아킴" 에게 배우는 댄스 입문',
-    count: {
-      current: 16,
-      total: 20,
-    },
   },
 ];
 
@@ -891,8 +376,6 @@ const dummyNotify: INotice[] = [
   },
 ];
 
-const dummyClassSelect = [{ label: 'asdasd', value: '18' }];
-
 const dummyPassTableData = [
   {
     user: {
@@ -913,19 +396,13 @@ const dummyPassTableData = [
 
 export {
   dummyPassTableData,
-  dummyClassSelect,
   dummyNotify,
   dummyUserInfo,
   dummyMain,
   dummyClass,
   dummyUserInputSuggestion,
   dummySearchData,
-  dummyImgURL,
-  dummyInstructor,
-  dummyClasstableData,
   dummyEnrollmentPerson,
-  dummyClassRoster,
-  dummyClassList,
   dummyIncomeList,
   dummyPaymentList,
 };
