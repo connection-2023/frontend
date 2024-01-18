c5eddc7bf4aa47495b7119f38e2e7ce5ef752684
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-01-19 03:11:13 +0900

    feat(#208): 메인화면 API 연결 및 로딩 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,78 @@
 import Link from 'next/link';
-import { dummyInstructor, dummyMain } from '@/constants/dummy';
-import { SmallLogoSVG, MainPopularSVG, MainTopSVG } from '@/icons/svg';
+import { Suspense } from 'react';
+import { MainPopularSVG, MainTopSVG } from '@/icons/svg';
+import { searchBestInstructor } from '@/lib/apis/serverApis/searchApis';
+import { useUserStore } from '@/store/userStore';
 import Banner from './_components/Banner';
-import CarouselTemplate from './_components/CarouselTemplate';
-import ClassCard from '@/components/ClassPreview/ClassPreview';
-import InstructorPreview from '@/components/Preview/InstructorPreview';
-import SuggestionPreview from '@/components/Preview/SuggestionPreview';
+import BestClass from './_components/Home/BestClass';
+import BestInstructor from './_components/Home/BestInstructor';
+import BestClassLoading from './_components/Home/Loading/BestClassLoading';
+import BestInstructorLoading from './_components/Home/Loading/BestInstructorLoading';
+import RecentClassLoading from './_components/Home/Loading/RecentClassLoading';
+import RecentClass from './_components/Home/RecentClass';
 
-const Home = () => {
-  const { classList } = dummyInstructor;
-  const { recentClass, suggestionClass, topInstructorList } = dummyMain;
+const Home = async () => {
+  const { userType } = useUserStore.getState();
+  const bestInstructorList = await searchBestInstructor(userType === 'user');
+
   return (
-    <main className="relative mx-auto mb-20">
+    <div className="relative mx-auto mb-20 w-full">
       <div className="h-[472px]">
         <Banner />
       </div>
 
       {/* 유저 로그인시에만 보여지는 맞춤 클래스 */}
-      <section className="absolute top-[23rem] mt-3 w-full">
-        <h2 className="mb-3 flex w-full items-center gap-1 px-4 text-lg font-bold text-white sm:px-9">
-          <SmallLogoSVG className="h-3 w-6" />
-          AI가 추천하는 맞춤 클래스
-        </h2>
+      {/* <SuggestionClass /> */}
 
-        <div className="flex gap-4 overflow-x-auto px-4 sm:px-9">
-          {suggestionClass.map((item) => (
-            <SuggestionPreview
-              key={item.title}
-              title={item.title}
-              range={item.range}
-              review={item.review}
-              image={item.image}
-            />
-          ))}
-        </div>
-      </section>
       {/* 인기 클래스 */}
-      <section className="mt-20 w-full">
+      <section className="mt-4 w-full">
         <SectionHeader
           icon={<MainPopularSVG />}
-          title="오늘의 인기 클래스"
+          title="인기 클래스"
           link="/class"
         />
 
-        <CarouselTemplate mode="class">
-          {classList.map((state, index) => (
-            <div
-              key={state.title + index}
-              className="w-full max-w-[13rem] xl:max-w-[33.7rem]"
-            >
-              <ClassCard key={state.title + index} {...state} />
-            </div>
-          ))}
-        </CarouselTemplate>
+        <Suspense fallback={<BestClassLoading />}>
+          {/* @ts-expect-error Async Server Component */}
+          <BestClass />
+        </Suspense>
       </section>
+
       {/* 인기 강사 TOP 8 */}
       <section className="mt-3">
         <SectionHeader
           icon={<MainTopSVG />}
-          title="오늘의 인기 강사 TOP8"
+          title="인기 강사 TOP8"
           link="/instructor"
         />
 
-        <CarouselTemplate mode="instructor">
-          {topInstructorList.map((list) => (
-            <InstructorPreview
-              key={list.id}
-              image={list.image}
-              nickname={list.nickname}
-            />
-          ))}
-        </CarouselTemplate>
+        <Suspense fallback={<BestInstructorLoading />}>
+          <BestInstructor bestInstructorList={bestInstructorList} />
+        </Suspense>
       </section>
-      {/* 최신 클래스 - 무한 스크롤 (예정) */}
+
+      {/* 최신 클래스 */}
+
       <section className="mt-3 px-4 sm:px-9">
         <h2 className="mb-4 flex w-full justify-start text-lg font-bold">
           최신 클래스
         </h2>
-        <div className="grid auto-cols-auto grid-cols-1 justify-items-center gap-x-4 gap-y-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-2 xl:gap-y-4">
-          {recentClass.map((item, i) => (
-            <div className="w-full flex-shrink flex-grow" key={i}>
-              <ClassCard {...item} />
-            </div>
-          ))}
-        </div>
+
+        <Suspense fallback={<RecentClassLoading />}>
+          {/* @ts-expect-error Async Server Component */}
+          <RecentClass />
+        </Suspense>
       </section>
-    </main>
+
+      <div className="mt-10 flex w-screen justify-center">
+        <Link
+          href="/class"
+          className="mx-auto rounded-[3.13rem] bg-main-color px-7 py-3 text-lg font-bold text-white"
+        >
+          더 많은 클래스 보러가기
+        </Link>
+      </div>
+    </div>
   );
 };
 
