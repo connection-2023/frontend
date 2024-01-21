58c912b5389796dcf6e1ab9a72bef28fa04feaa8
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-01-21 20:50:30 +0900

    refactor(#209): 변경 불가능한 컴포넌트 분리

---
+++ src/app/class/[id]/edit/_components/NonEditableSection.tsx
@@ -0,0 +1,63 @@
+import { IClassEditPageData } from '@/types/class';
+import { CLASS_EDIT_STYLE } from '@/constants/constants';
+import {
+  LocationSVG,
+  TimeSVG,
+  GroupSVG,
+  LevelSVG,
+  GenreSVG,
+} from '@/icons/svg';
+import {
+  formatLocationToString,
+  formatGenreToString,
+} from '@/utils/parseUtils';
+
+const NonEditableSection = (props: IClassEditPageData) => {
+  const {
+    title,
+    lectureToRegion,
+    lectureToDanceGenre,
+    duration,
+    isGroup,
+    maxCapacity,
+    difficultyLevel,
+  } = props;
+
+  return (
+    <>
+      <h1 className="relative flex w-full max-w-[40rem] px-4 text-lg font-bold md:justify-center">
+        <p className="w-11/12 md:text-center">{title}</p>
+      </h1>
+
+      <hr className="mb-4 h-1 w-full max-w-[40rem] md:mb-6" />
+
+      <div className="mb-4 grid w-full max-w-[40rem] grid-cols-2 gap-y-3.5 px-4 md:mb-7 md:flex md:flex-wrap md:justify-items-center md:gap-x-10 md:whitespace-nowrap">
+        <h3 className={CLASS_EDIT_STYLE.h3}>
+          <LocationSVG />
+          <span className="w-fit break-keep">
+            {formatLocationToString(lectureToRegion)}
+          </span>
+        </h3>
+        <h3 className={CLASS_EDIT_STYLE.h3}>
+          <GenreSVG />
+          <span className="w-fit break-keep">
+            {formatGenreToString(lectureToDanceGenre)}
+          </span>
+        </h3>
+
+        <h3 className={CLASS_EDIT_STYLE.h3}>
+          <TimeSVG /> {duration}분
+        </h3>
+        <h3 className={CLASS_EDIT_STYLE.h3}>
+          <GroupSVG />
+          {isGroup ? `그룹레슨 (${maxCapacity}인)` : '개인레슨'}
+        </h3>
+        <h3 className={CLASS_EDIT_STYLE.h3}>
+          <LevelSVG /> {difficultyLevel}
+        </h3>
+      </div>
+    </>
+  );
+};
+
+export default NonEditableSection;
