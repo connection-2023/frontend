667cb90f9e959a627f137520701fa97a9345e94d
hyeonjun-L <beeminor60@gmail.com>
2024-01-24 06:29:30 +0900

    feat(#240): 알림/이메일 수신 동의 로직 간략 구현

--- src/app/mypage/instructor/info/page.tsx
+++ src/app/mypage/instructor/info/page.tsx
@@ -1,6 +1,6 @@
-import Button from '@/components/Button/Button';
 import MyInfo from './_components/MyInfo';
 import Account from './_components/Account';
+import ConsentReceiptModification from '@/components/ConsentReceiptModification/ConsentReceiptModification';
 
 const page = () => {
   return (
@@ -10,27 +10,7 @@ const page = () => {
           <MyInfo />
           <Account />
         </div>
-        <article className="flex w-full flex-shrink-0 flex-col self-start whitespace-nowrap rounded-md bg-white p-5 px-5 py-6 shadow-vertical lg:w-80">
-          <ul className="mb-5 flex flex-col gap-5 px-1">
-            <h1 className="text-xl font-bold">알림/이메일 수신 동의</h1>
-            <li className="mb-1 flex items-center gap-3">
-              <input
-                id="ex"
-                type="checkbox"
-                className="h-[18px] w-[18px] accent-black"
-              />
-              <label htmlFor="ex" className="cursor-pointer text-sm">
-                알림톡 수신 동의
-              </label>
-              <button className="text-gray-500 underline">보기</button>
-            </li>
-          </ul>
-          <div className="w-full sm:ml-auto sm:w-28 lg:w-full">
-            <Button color="secondary" disabled={true}>
-              적용하기
-            </Button>
-          </div>
-        </article>
+        <ConsentReceiptModification />
       </div>
     </main>
   );
