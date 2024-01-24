9834e751d8487c8b655c2713668559c7f3397307
hyeonjun-L <beeminor60@gmail.com>
2024-01-24 16:25:56 +0900

    refactor(#240): 공통 모달 컴포넌트 children 1개만 되도록 리팩토링

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,14 +1,19 @@
 'use client';
-import { useEffect, useRef } from 'react';
-import MobileModal from './MobileModal';
+import { useEffect, useRef, useState } from 'react';
 import { CloseSVG } from '../../../public/icons/svg';
+import useBottomSheet from '@/hooks/useBottomSheet';
+import { motion } from 'framer-motion';
+import useMediaQuery from '@/hooks/useMediaQuery';
 
 interface ModalProps {
   children: React.ReactNode;
   isOpened: boolean;
   handleClosed: () => void;
 }
+
 const Modal = ({ children, isOpened, handleClosed }: ModalProps) => {
+  const isSm = useMediaQuery('(min-width: 640px)');
+  const { onDragEnd, controls } = useBottomSheet(handleClosed, isOpened);
   const overlayRef = useRef(null);
 
   const handleKeyUp = (e: KeyboardEvent) => {
@@ -19,35 +24,56 @@ const Modal = ({ children, isOpened, handleClosed }: ModalProps) => {
   useEffect(() => {
     window.addEventListener('keyup', handleKeyUp);
 
-    return () => window.removeEventListener('keyup', handleKeyUp);
+    return () => {
+      window.removeEventListener('keyup', handleKeyUp);
+    };
   }, []);
 
   return isOpened ? (
-    <>
-      <div
-        ref={overlayRef}
-        className="fixed bottom-0 left-0 right-0 top-0 z-modal mx-auto bg-black/60 backdrop-blur-sm"
-        onClick={(e) => {
-          if (overlayRef.current !== e.target) return;
-          handleClosed();
+    <div
+      ref={overlayRef}
+      className="fixed bottom-0 left-0 right-0 top-0 z-modal mx-auto bg-black/60 backdrop-blur-sm"
+      onClick={(e) => {
+        if (overlayRef.current !== e.target) return;
+        handleClosed();
+      }}
+    >
+      <motion.div
+        drag={isSm ? false : 'y'}
+        onDragEnd={isSm ? undefined : onDragEnd}
+        initial={isSm ? false : 'hidden'}
+        animate={isSm ? undefined : controls}
+        transition={{
+          type: 'spring',
+          damping: 40,
+          stiffness: 400,
         }}
+        variants={{
+          visible: { y: 0 },
+          hidden: { y: '100%' },
+        }}
+        dragConstraints={{ top: 0 }}
+        dragElastic={0.2}
+        className={`absolute bottom-0 z-modal h-[90%] w-screen rounded-t-lg bg-white pt-2.5 sm:left-1/2 sm:top-1/2 sm:h-auto sm:w-auto sm:-translate-x-1/2 sm:-translate-y-1/2 sm:rounded-md sm:pt-0 sm:shadow-float`}
       >
-        <div className="absolute left-1/2 top-1/2 hidden h-auto w-auto -translate-x-1/2 -translate-y-1/2 rounded-md bg-white shadow-float sm:block">
-          <button onClick={handleClosed} className="absolute right-2 top-2">
-            <CloseSVG
-              width="24"
-              height="24"
-              className="stroke-gray-500 stroke-2"
-            />
-          </button>
+        <button
+          onClick={handleClosed}
+          className="absolute right-2 top-2 hidden sm:block"
+        >
+          <CloseSVG
+            width="24"
+            height="24"
+            className="stroke-gray-500 stroke-2"
+          />
+        </button>
 
-          {children}
+        <div className="mb-8 flex w-full justify-center sm:hidden">
+          <button className="h-1.5 w-16 rounded-lg bg-gray-700" />
         </div>
-      </div>
-      <MobileModal isOpened={isOpened} handleClosed={handleClosed}>
+
         {children}
-      </MobileModal>
-    </>
+      </motion.div>
+    </div>
   ) : null;
 };
 
