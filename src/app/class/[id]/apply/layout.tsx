9bc97164366de965f99f4d09f575a828051929f6
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-01-29 19:11:09 +0900

    feat(#246): 클래스 신청 페이지 변경된 UI 적용

--- src/app/class/[id]/apply/layout.tsx
+++ src/app/class/[id]/apply/layout.tsx
@@ -10,12 +10,5 @@ export default function ClassCreateLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <h1 className="mx-auto mb-6 flex w-full items-center justify-center border-b border-solid border-gray-700 py-4 text-2xl font-bold">
-        클래스 신청하기
-      </h1>
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
