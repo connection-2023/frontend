a5d61619b2604f11fb91272bf6d4c0d7384e90b0
hyeonjun-L <beeminor60@gmail.com>
2024-01-30 20:20:08 +0900

    refactor(#240): 프로필 업데이트 중복코드 리팩토링

--- src/components/MyInfo/UpdateModal/UpdateList/ProfileUpdate.tsx
+++ src/components/MyInfo/UpdateModal/UpdateList/ProfileUpdate.tsx
@@ -24,36 +24,57 @@ const ProfileUpdate = ({ profileImage }: ProfileUpdateProps) => {
               <ProfileSVG className="h-full w-full rounded-full border-2 border-black" />
             </div>
           )}
-          <div className="group absolute -bottom-[50%] right-1/2 flex translate-x-1/2 items-center gap-3 sm:-bottom-10 sm:right-11 sm:translate-x-0">
-            <button className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-full bg-white shadow-vertical group-hover:shadow-[inset_0_0px_3px_1px_rgba(0,0,0,0.3)]">
-              <CloseSVG
-                width="21"
-                height="21"
-                className="stroke-black stroke-2"
-              />
-            </button>
-            <label className="cursor-pointer text-white group-hover:font-semibold sm:hidden">
-              편집 취소
-            </label>
-          </div>
-          <div className="group absolute -bottom-1/3 right-1/2 flex translate-x-1/2 items-center gap-3 sm:-right-3 sm:bottom-0 sm:translate-x-0">
-            <button className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-full bg-white shadow-vertical group-hover:shadow-[inset_0_0px_3px_1px_rgba(0,0,0,0.3)]">
-              <DeleteSVG width="28" height="28" className="stroke-black" />
-            </button>
-            <label className="cursor-pointer text-white group-hover:font-semibold sm:hidden">
-              사진 삭제
-            </label>
-          </div>
-          <div className="group absolute -bottom-[17%] right-1/2 flex translate-x-1/2 items-center gap-3 sm:-right-12 sm:bottom-14 sm:translate-x-0">
-            <button className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-full bg-white shadow-vertical group-hover:shadow-[inset_0_0px_3px_1px_rgba(0,0,0,0.3)]">
-              <ChangeImageSVG className="h-7 w-7" />
-            </button>
-            <label className="cursor-pointer text-white group-hover:font-semibold sm:hidden">
-              사진 변경
-            </label>
-          </div>
+          <ButtonGroup
+            icon={CloseSVG}
+            label="편집 취소"
+            positionClasses="-bottom-[50%] right-1/2 sm:-bottom-10 sm:right-11"
+            iconProps={{
+              width: '21',
+              height: '21',
+              className: 'stroke-black stroke-2',
+            }}
+          />
+          <ButtonGroup
+            icon={DeleteSVG}
+            label="사진 삭제"
+            positionClasses="-bottom-1/3 right-1/2 sm:-right-3 sm:bottom-0"
+            iconProps={{ width: '28', height: '28', className: 'stroke-black' }}
+          />
+          <ButtonGroup
+            icon={ChangeImageSVG}
+            label="사진 변경"
+            positionClasses="-bottom-[17%] right-1/2 sm:-right-12 sm:bottom-14"
+            iconProps={{ className: 'h-7 w-7' }}
+          />
         </div>
       </div>
+    </div>
+  );
+};
+
+interface ButtonGroupProps {
+  icon: React.FC<React.SVGProps<SVGSVGElement>>;
+  label: string;
+  positionClasses: string;
+  iconProps: React.SVGProps<SVGSVGElement>;
+}
+
+const ButtonGroup = ({
+  icon: Icon,
+  label,
+  positionClasses,
+  iconProps,
+}: ButtonGroupProps) => {
+  return (
+    <div
+      className={`group absolute ${positionClasses} flex translate-x-1/2 items-center gap-3 sm:translate-x-0`}
+    >
+      <button className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-full bg-white shadow-vertical group-hover:shadow-[inset_0_0px_3px_1px_rgba(0,0,0,0.3)]">
+        <Icon {...iconProps} />
+      </button>
+      <label className="cursor-pointer text-white group-hover:font-semibold sm:hidden">
+        {label}
+      </label>
     </div>
   );
 };
