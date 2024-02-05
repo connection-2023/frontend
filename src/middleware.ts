ab4fc5b969c3be44b22779578d07b3f1a584ea6e
hyeonjun-L <beeminor60@gmail.com>
2024-02-06 05:47:09 +0900

    fix(#264): 미들웨어 프로덕션에서 동작 안하는 버그 해결

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 import {
   LECTURER_NO_ACCESS,
   LOGIN_REQUIRED_URLS,
@@ -20,21 +20,36 @@ const setCookie = (response: NextResponse, name: string, value: string) => {
   });
 };
 
-export const middleware = async (request: NextRequest) => {
+const handleInvalidToken = (request: NextRequest, includes: boolean) => {
+  const response = includes
+    ? NextResponse.redirect(new URL('/login', request.url))
+    : NextResponse.redirect(request.url);
+
+  if (!includes) response.cookies.set('reload', 'true');
+
+  response.cookies.delete('userAccessToken');
+  response.cookies.delete('lecturerAccessToken');
+  response.cookies.delete('refreshToken');
+
+  return response;
+};
+
+export async function middleware(request: NextRequest, event: NextFetchEvent) {
   const user = request.cookies.get('userAccessToken')?.value;
   const lecturer = request.cookies.get('lecturerAccessToken')?.value;
+  const authorization = user || lecturer;
 
-  if (user || lecturer) {
+  if (authorization) {
     try {
       if (user) {
-        await checkAccessToken('userAccessToken');
+        await checkAccessToken('user', authorization);
 
         if (USER_NO_ACCESS.includes(request.nextUrl.pathname)) {
           // 유저가 가면 안되는 lecturer 링크
           return NextResponse.redirect(new URL('/', request.url));
         }
       } else if (lecturer) {
-        await checkAccessToken('lecturerAccessToken');
+        await checkAccessToken('lecturer', authorization);
 
         if (LECTURER_NO_ACCESS.includes(request.nextUrl.pathname)) {
           // 강사가 가면 안되는 user 링크 확인
@@ -52,49 +67,55 @@ export const middleware = async (request: NextRequest) => {
       if (error instanceof Error) {
         const fetchError = error as FetchError;
         if (fetchError.status === 401) {
+          const currentRefreshToken =
+            request.cookies.get('refreshToken')?.value;
           try {
-            const response = NextResponse.redirect(request.url);
+            if (currentRefreshToken) {
+              const response = NextResponse.redirect(request.url);
 
-            const { accessToken, refreshToken } = await accessTokenReissuance();
+              const { accessToken, refreshToken } = await (
+                await accessTokenReissuance(currentRefreshToken)
+              ).json();
 
-            const tokenName = user ? 'userAccessToken' : 'lecturerAccessToken';
+              const tokenName = user
+                ? 'userAccessToken'
+                : 'lecturerAccessToken';
 
-            setCookie(response, tokenName, accessToken);
-            setCookie(response, 'refreshToken', refreshToken);
+              setCookie(response, tokenName, accessToken);
+              setCookie(response, 'refreshToken', refreshToken);
 
-            return response;
+              return response;
+            } else {
+              throw new Error('refreshToken 존재하지 않음');
+            }
           } catch (error) {
+            console.error(error);
             const includes = LOGIN_REQUIRED_URLS.includes(
               request.nextUrl.pathname,
             );
+            // 로그인이 필요한 링크 (강사 || 유저)
 
-            const response = includes
-              ? NextResponse.redirect(new URL('/login', request.url))
-              : NextResponse.redirect(request.url);
-
-            if (!includes) response.cookies.set('reload', 'true');
-
-            response.cookies.delete('userAccessToken');
-            response.cookies.delete('lecturerAccessToken');
-            response.cookies.delete('refreshToken');
-
-            return response;
+            return handleInvalidToken(request, includes);
           }
+        } else {
+          const includes = LOGIN_REQUIRED_URLS.includes(
+            request.nextUrl.pathname,
+          );
+          // 로그인이 필요한 링크 (강사 || 유저)
+
+          return handleInvalidToken(request, includes);
         }
       }
-      return NextResponse.redirect(new URL('/', request.url)); //추후 서버 에러 페이지로 이동
     }
   }
 
   if (LOGIN_REQUIRED_URLS.includes(request.nextUrl.pathname)) {
-    //강사 토큰이 필요한 링크
-    //유저 토큰이 필요한 링크
-    // 로그인이 필요한 링크
+    // 로그인이 필요한 링크 (강사 || 유저)
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
   return NextResponse.next();
-};
+}
 
 export const config = {
   matcher: [
