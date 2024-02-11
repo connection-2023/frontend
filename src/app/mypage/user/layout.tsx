1b4ded9b51cf52dc33b65766469dd38810efc808
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-02-11 17:21:31 +0900

    feat(#250): 유저 마이페이지 사이드바 스타일 변경

--- src/app/mypage/user/layout.tsx
+++ src/app/mypage/user/layout.tsx
@@ -13,7 +13,7 @@ export default async function MyPageLayout({
 }) {
   return (
     <section className="grid h-max w-full flex-1 grid-cols-1 justify-between gap-x-4 pb-20 pt-4 xl:grid-cols-[1fr,3fr] xl:px-5">
-      <aside className="hidden w-full rounded-lg bg-white shadow-float xl:block">
+      <aside className="hidden w-full xl:block">
         <Sidebar />
       </aside>
       {children}
