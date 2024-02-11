dc0e5cb528e4c0f70ab9d49d73f2e25307c2e037
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-02-12 01:02:12 +0900

    feat(#276): 수익 차트 API 로직 구현

---
+++ src/lib/apis/incomeApis.ts
@@ -0,0 +1,22 @@
+export const getIncomeStatics = async (type: 'MONTHLY' | 'DAILY') => {
+  const query =
+    type === 'DAILY'
+      ? `statisticsType=${type}&date=${new Date()}`
+      : `statisticsType=${type}`;
+
+  try {
+    const response = await fetch(`/api/income/stat?${query}`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((data) => data.json());
+
+    return response.data.revenueStatistics;
+  } catch (error) {
+    if (error instanceof Error && error.message) {
+      return error.message;
+    }
+  }
+};
