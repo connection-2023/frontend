8a209c5b143c3cce4fb9e198b995bb06b1af7c23
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-02-19 16:33:19 +0900

    feat(#163): 신청한 클래스 상세보기 페이지 구현

---
+++ src/app/mypage/user/myclass/apply/[id]/page.tsx
@@ -0,0 +1,75 @@
+import Link from 'next/link';
+import Back from './_components/Back';
+import CancelApplicationButton from './_components/CancelApplicationButton';
+import ClassLocation from './_components/ClassLocation';
+import OneDayClass from './_components/OneDayClass';
+import RegularClass from './_components/RegularClass';
+import Notice from '@/components/ClassNotice/Notice';
+import { ButtonStyles } from '@/constants/constants';
+import { getApplyClassDetail } from '@/lib/apis/serverApis/classApi';
+
+const ApplyDetailPage = async ({
+  params: { id },
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: { [key: string]: string };
+}) => {
+  const { type } = searchParams;
+
+  if (type !== '원데이' && type !== '정기')
+    throw Error('type(원데이/정기)가 지정되지 않았습니다!');
+
+  const data = await getApplyClassDetail(id, type);
+
+  const { lecture } = data;
+
+  return (
+    <section className="mb-18 mx-auto flex h-full w-full max-w-[40rem] flex-col rounded-lg px-4 text-sm md:px-0 xl:mx-0">
+      <div className="flex flex-col items-center whitespace-nowrap border-solid border-gray-500 py-3 md:flex-row md:border-b">
+        <div className="mb-2.5 flex w-full border-b border-solid border-gray-500 py-3 md:mb-0 md:border-none md:py-0">
+          <Back />
+          <p className="text-2xl font-bold">{lecture.title}</p>
+        </div>
+
+        <div className="flex w-full gap-2 md:w-fit">
+          <CancelApplicationButton {...data} />
+
+          <div className="w-1/2 md:hidden">
+            <Link
+              href={`/class/${lecture.id}#intro-section`}
+              className={`h-[35px] font-semibold ${ButtonStyles.default}`}
+            >
+              커리큘럼
+            </Link>
+          </div>
+          <div className="hidden w-[4.69rem] md:block">
+            <Link
+              href={`/class/${lecture.id}#intro-section`}
+              className={`h-7 font-semibold ${ButtonStyles.default}`}
+            >
+              커리큘럼
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="mb-4 mt-2 md:mt-3">
+        <Notice
+          content={lecture.notification.content}
+          updateDate={lecture.notification.updatedAt}
+        />
+      </div>
+
+      {type === '원데이' ? (
+        <OneDayClass {...data} />
+      ) : (
+        <RegularClass {...data} />
+      )}
+
+      <ClassLocation {...data} />
+    </section>
+  );
+};
+
+export default ApplyDetailPage;
