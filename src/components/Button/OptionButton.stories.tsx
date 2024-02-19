7b1d2e24ba0299f24f50b1a9393a152012294f90
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-02-20 02:18:57 +0900

    feat(#285): defaultValue 변경

--- src/components/Button/OptionButton.stories.tsx
+++ src/components/Button/OptionButton.stories.tsx
@@ -6,13 +6,10 @@ const meta: Meta<typeof OptionButtons> = {
   component: OptionButtons,
   parameters: {
     layout: 'centered',
-    parameters: {
-      layout: 'centered',
-      docs: {
-        description: {
-          component:
-            '클래스/강사 상세페이지에서 사용되는 공유하기, 신고하기, 차단하기 옵션의 옵션 버튼 컴포넌트',
-        },
+    docs: {
+      description: {
+        component:
+          '클래스/강사 상세페이지에서 사용되는 공유하기, 신고하기, 차단하기 옵션의 옵션 버튼 컴포넌트',
       },
     },
   },
@@ -20,17 +17,14 @@ const meta: Meta<typeof OptionButtons> = {
   argTypes: {
     title: {
       description: '공유하기 제목',
-      defaultValue: { summary: 'Connection 댄스 클래스' },
       control: 'text',
     },
     lecturerId: {
       description: '클래스 및 강사 신고/차단 시 강사 id',
-      defaultValue: { summary: 1 },
       control: 'number',
     },
     mode: {
-      description: 'mode',
-      defaultValue: { summary: '공유하기 모드' },
+      description: '공유하기 모드',
       options: ['class', 'instructor'],
       control: { type: 'radio' },
     },
@@ -44,8 +38,12 @@ export const Default: Story = {
   args: {
     mode: 'class',
     title: 'Connection 댄스 클래스',
-    lecturerId: 1,
+    lecturerId: 0,
   },
 
-  render: (args) => <OptionButtons {...args} />,
+  render: (args) => (
+    <div className="h-32 w-full">
+      <OptionButtons {...args} />
+    </div>
+  ),
 };
