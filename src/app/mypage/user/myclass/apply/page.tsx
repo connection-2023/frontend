514e8229ce5031672e6b41ebbdaae48c0faaead1
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-02-19 16:30:14 +0900

    feat(#163): 유저 신청한 클래스 페이지 구현

--- src/app/mypage/user/myclass/apply/page.tsx
+++ src/app/mypage/user/myclass/apply/page.tsx
@@ -1,142 +1,71 @@
 'use client';
 import dynamic from 'next/dynamic';
-import { useState, useEffect } from 'react';
-import { getUserClass } from '@/lib/apis/classApis';
-import Pagination from '@/components/Pagination/Pagination';
-import PageSizeSelector from '@/components/Selector/PageSizeSelector';
-import { IUserClassResponse } from '@/types/class';
+import { useState } from 'react';
+import { BasicCalendarSVG } from '@/icons/svg';
+import CalendarView from './_components/CalendarView';
 
-const ClassList = dynamic(() => import('./_components/ClassList'), {
+const ListView = dynamic(() => import('./_components/ListView'), {
   ssr: false,
 });
 
 const ClassListView = () => {
-  const [activeTab, setActiveTab] = useState('진행중');
-  const [displayCount, setDisplayCount] = useState(5);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [itemsId, setItemsId] = useState({
-    firstItemId: 0,
-    lastItemId: 0,
-  });
-  const [classListData, setClassListData] = useState<IUserClassResponse[]>([]);
+  const [activeView, setActiveView] = useState<'calendar' | 'list'>('calendar');
 
-  useEffect(() => {
-    // fetchClassData(
-    //   displayCount,
-    //   0,
-    //   itemsId.firstItemId,
-    //   itemsId.lastItemId,
-    //   activeTab,
-    // );
-  }, []);
-
-  const fetchClassData = async (
-    displayCount: number,
-    targetPage: number,
-    firstItemId: number,
-    lastItemId: number,
-    isProgress: boolean,
-  ) => {
-    const option = isProgress ? '진행중' : '수강 완료';
-    const classLists = await getUserClass(
-      displayCount,
-      currentPage,
-      targetPage,
-      firstItemId,
-      lastItemId,
-      option,
-    );
-
-    if (classLists.enrollLecture.length) {
-      setClassListData(classLists.enrollLecture);
-      setCurrentPage(targetPage);
-
-      setItemsId({
-        firstItemId: classLists.enrollLecture[0].id,
-        lastItemId: classLists.enrollLecture[displayCount - 1].id,
-      });
-    } else {
-      setClassListData([]);
-    }
+  const handleActiveView = (view: 'calendar' | 'list') => {
+    if (activeView !== view) setActiveView(view);
   };
 
-  const handleActiveTab = (newStatus: string) => {
-    setActiveTab(newStatus);
-
-    // fetchClassData(
-    //   displayCount,
-    //   currentPage,
-    //   itemsId.firstItemId,
-    //   itemsId.lastItemId,
-    //   !isProgress,
-    // );
-  };
-
-  const handleDisplayCount = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const newDisplayCount = Number(event.target.value);
-
-    setDisplayCount(newDisplayCount);
-
-    // fetchClassData(
-    //   newDisplayCount,
-    //   currentPage,
-    //   itemsId.firstItemId,
-    //   itemsId.lastItemId,
-    //   activeTab,
-    // );
-  };
-
-  const handlePageChange = async ({ selected }: { selected: number }) => {
-    setCurrentPage(selected);
-
-    // fetchClassData(
-    //   displayCount,
-    //   selected,
-    //   itemsId.firstItemId,
-    //   itemsId.lastItemId,
-    //   activeTab,
-    // );
-  };
+  const getButtonStyle = (view: 'calendar' | 'list') =>
+    activeView === view
+      ? 'flex items-center justify-center rounded-md border border-solid border-black bg-black h-9 gap-2 px-2 py-1 text-white font-bold'
+      : 'flex items-center justify-center rounded-md border border-solid border-black bg-white h-9 gap-2 px-2 py-1 text-gray-100 font-medium';
 
   return (
-    <section className="mx-auto flex w-full max-w-[40rem] flex-col bg-white px-4 py-5 text-sm text-gray-100 md:px-9 xl:mx-0 xl:px-0">
-      <h1 className="mb-4 border-b border-solid border-gray-700 pb-2.5 text-2xl font-bold">
-        신청한 클래스
-      </h1>
+    <section className="flex flex-col bg-white px-4 py-5 text-sm text-gray-100 md:px-9 xl:mx-0 xl:px-0">
+      <div className="mb-4 flex items-center justify-between border-b border-solid border-gray-700 pb-2.5 md:justify-normal">
+        <h1 className="text-2xl font-bold">신청한 클래스</h1>
 
-      <div className="mb-4 flex w-full items-center justify-between">
-        <ul className="flex gap-4 whitespace-nowrap text-base font-medium text-gray-300">
-          {['진행중', '대기', '수강 완료', '취소/거절'].map((item) => (
-            <li
-              key={item}
-              onClick={() => handleActiveTab(item)}
-              className={`${
-                activeTab === item
-                  ? 'cursor-pointer font-bold text-black'
-                  : 'cursor-pointer'
-              }`}
-            >
-              {item}
-            </li>
-          ))}
-        </ul>
+        <div className="ml-4 flex gap-2 whitespace-nowrap text-base">
+          <button
+            onClick={() => {
+              handleActiveView('calendar');
+            }}
+            className={getButtonStyle('calendar')}
+          >
+            <BasicCalendarSVG
+              width="21"
+              height="21"
+              className={
+                activeView === 'calendar' ? 'fill-white' : 'fill-gray-100'
+              }
+            />
+            달력
+          </button>
 
-        <PageSizeSelector value={displayCount} onChange={handleDisplayCount} />
+          <button
+            onClick={() => {
+              handleActiveView('list');
+            }}
+            className={getButtonStyle('list')}
+          >
+            <div className="flex h-6 w-6 flex-col items-center justify-between gap-0.5 p-1">
+              <div
+                className={`h-5 w-full border border-${
+                  activeView === 'list' ? 'white' : 'black'
+                } rounded-sm`}
+              />
+              <div
+                className={`h-5 w-full border border-${
+                  activeView === 'list' ? 'white' : 'black'
+                } rounded-sm`}
+              />
+            </div>
+            목록
+          </button>
+        </div>
       </div>
 
-      <ul className="mb-9 flex flex-col gap-4">
-        {classListData.map((item) => (
-          <ClassList key={item.id} {...item} activeTab={activeTab} />
-        ))}
-      </ul>
-
-      <div className="w-full">
-        <Pagination
-          pageCount={5} // 추후 계산 필요
-          currentPage={currentPage}
-          onPageChange={handlePageChange}
-        />
-      </div>
+      {activeView === 'calendar' ? <CalendarView /> : <ListView />}
     </section>
   );
 };
