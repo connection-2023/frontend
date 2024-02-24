2fefb79646105b588ead796678ba630a753e95a3
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-02-24 17:24:34 +0900

    feat(#266): 레이아웃 스타일 변경

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,8 +11,8 @@ export default async function MyPageLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="mx-auto mb-20 grid w-full auto-rows-max grid-cols-1 bg-main-color-transparent pb-12 md:grid-cols-2 md:gap-4 md:px-[2.25rem] xl:grid-cols-4">
+    <div className="mx-auto mb-20 grid w-full grid-cols-1 grid-rows-[min-content_max-content] bg-main-color-transparent pb-12 md:grid-cols-2 md:gap-4 md:px-9 xl:grid-cols-4">
       {children}
-    </main>
+    </div>
   );
 }
