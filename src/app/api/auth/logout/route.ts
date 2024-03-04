a8eca4cbfe45d055f06652d91124d83046016026
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-03-05 02:12:41 +0900

    fix(#313): 로그아웃 버그 수정

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,32 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export const GET = async (request: NextRequest) => {
+  const token =
+    request.cookies.get('userAccessToken')?.value ||
+    request.cookies.get('lecturerAccessToken')?.value;
+
+  if (!token) {
+    return new NextResponse(
+      JSON.stringify({ status: 401, message: '로그인된 유저가 아닙니다!' }),
+    );
+  }
+
   try {
-    const response = NextResponse.json({}, { status: 200 });
+    const clientResponse = new NextResponse();
 
-    response.cookies.set({
-      name: 'userAccessToken',
-      value: '',
-      httpOnly: true,
-      path: '/',
-      maxAge: 0,
-    });
-    response.cookies.set({
-      name: 'lecturerAccessToken',
-      value: '',
-      httpOnly: true,
-      path: '/',
-      maxAge: 0,
-    });
-    response.cookies.set({
-      name: 'refreshToken',
-      value: '',
-      httpOnly: true,
-      path: '/',
-      maxAge: 0,
-    });
+    clientResponse.cookies.delete('userAccessToken');
+    clientResponse.cookies.delete('lecturerAccessToken');
+    clientResponse.cookies.delete('refreshToken');
 
-    return response;
+    return clientResponse;
   } catch (error) {
     return new NextResponse(
       JSON.stringify({ status: 500, message: '로그아웃에 실패하였습니다' }),
