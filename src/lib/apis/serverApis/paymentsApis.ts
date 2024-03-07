f5e56b10b90ad4914b4579b0a8ed1d4dd4dbba9f
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-03-07 13:46:41 +0900

    refactor(#315): 결제 승인/결과 조회 로직 분리

--- src/lib/apis/serverApis/paymentsApis.ts
+++ src/lib/apis/serverApis/paymentsApis.ts
@@ -8,39 +8,54 @@ import { FetchError } from '@/types/types';
 
 const END_POINT = process.env.NEXT_PUBLIC_API_END_POINT;
 
-export const patchPaymentConfirm = async (
-  data: IPaymentConfirmRequest,
-): Promise<IPaymentConfirmResponse | Error> => {
+export const patchPaymentConfirm = async (data: IPaymentConfirmRequest) => {
   const cookieStore = cookies();
   const token = cookieStore.get('userAccessToken')?.value;
 
   if (!token) throw new Error(`유저 토큰 에러!`);
 
-  try {
-    const response = await fetch(END_POINT + '/payments/toss/confirm', {
-      method: 'PATCH',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(data),
-    });
+  const response = await fetch(END_POINT + '/payments/toss/confirm', {
+    method: 'PATCH',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  });
 
-    if (!response.ok) {
-      const errorData = await response.json();
-      const error: FetchError = new Error(errorData.message || '');
-      error.status = response.status;
-      throw new Error(`결제 승인 오류: ${error.status} ${error}`);
-    }
+  const resData = await response.json();
 
-    const resData = await response.json();
+  return resData;
+};
 
-    return resData.data;
-  } catch (error) {
-    console.error('결제 승인 오류', error);
-    throw error;
+export const getPaymentInfo = async (
+  orderId: string,
+): Promise<IPaymentConfirmResponse> => {
+  const cookieStore = cookies();
+  const token = cookieStore.get('userAccessToken')?.value;
+
+  if (!token) throw new Error(`유저 토큰 에러!`);
+
+  const response = await fetch(END_POINT + `/payments/${orderId}`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    const error: FetchError = new Error(errorData.message || '');
+    error.status = response.status;
+    throw new Error(`결제 승인 오류: ${error.status} ${error}`);
   }
+
+  const { data } = await response.json();
+
+  return data.paymentResult;
 };
 
 export const getReceipt = async (
