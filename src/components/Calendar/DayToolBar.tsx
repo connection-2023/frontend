293daeffbee44e69c31cecfdc7f56b9f47467a76
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-03-08 11:35:21 +0900

    feat(#322): 달력 툴바 아이콘 변경

--- src/components/Calendar/DayToolBar.tsx
+++ src/components/Calendar/DayToolBar.tsx
@@ -1,22 +1,23 @@
 import { View, ToolbarProps } from 'react-big-calendar';
-import { ArrowRightSVG } from '@/icons/svg';
+import { ArrowUpSVG } from '@/icons/svg';
 
 interface IToolbarProps extends Partial<ToolbarProps> {
   label: string;
   view: View;
+  // eslint-disable-next-line no-unused-vars
   onNavigate: (action: 'PREV' | 'NEXT' | 'TODAY' | 'DATE') => void;
 }
 
 const DayToolBar = ({ label, onNavigate }: IToolbarProps) => (
-  <div className="mb-[0.69rem] flex items-center justify-between gap-4 whitespace-nowrap">
+  <div className="mb-2.5 flex items-center justify-between gap-4 whitespace-nowrap">
     <h2 className="text-lg font-semibold text-gray-100">{label}</h2>
-    <div className="flex h-[1.875rem] w-32 divide-x divide-solid overflow-hidden rounded-[0.4rem] border border-solid border-gray-500">
+    <div className="flex h-[1.875rem] w-32 divide-x divide-solid overflow-hidden rounded-md border border-solid border-gray-500">
       <button
         aria-label="이전 날짜로 이동"
         onClick={() => onNavigate('PREV')}
         className={`h-7 origin-center rotate-180 ${NavButtonStyle}`}
       >
-        <ArrowRightSVG className="h-[5px] w-[9px] stroke-black" />
+        <ArrowUpSVG className="h-7 w-full origin-center rotate-90 fill-black" />
       </button>
       <button
         aria-label="오늘 날짜로 이동"
@@ -30,7 +31,7 @@ const DayToolBar = ({ label, onNavigate }: IToolbarProps) => (
         onClick={() => onNavigate('NEXT')}
         className={`h-7 ${NavButtonStyle}`}
       >
-        <ArrowRightSVG className="h-[5px] w-[9px] stroke-black" />
+        <ArrowUpSVG className="h-7 w-full origin-center rotate-90 fill-black" />
       </button>
     </div>
   </div>
