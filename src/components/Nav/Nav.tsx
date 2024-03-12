d93d45e89ab8ecc2661eeea2809bc5d93f2a61cd
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2024-03-12 22:51:38 +0900

    refactor(#332): 활성화 섹션 계산 로직 리팩토링

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Link from 'next/link';
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 interface NavSection {
   id: string;
@@ -8,82 +8,52 @@ interface NavSection {
 }
 
 const Nav = ({ sections }: { sections: NavSection[] }) => {
-  const [activeId, setActiveId] = useState<null | string>(null);
-  const [headingTops, setHeadingTops] = useState<
-    | null
-    | {
-        id: string;
-        top: number;
-      }[]
-  >(null);
+  const [activeSection, setActiveSection] = useState('');
+  const sectionRefs = useRef<{ [key: string]: HTMLElement | null }>({});
 
-  const updateSectionPositions = useCallback(() => {
-    const scrollTop = getScrollTop();
-    const headingTops = sections.map(({ id }) => {
-      const el = document.getElementById(id);
-
-      if (!el) {
-        return {
-          id,
-          top: 0,
-        };
-      }
-      const top = el.getBoundingClientRect().top + scrollTop;
-
-      return {
-        id,
-        top,
-      };
+  useEffect(() => {
+    sections.forEach((list) => {
+      const elem = document.getElementById(list.id);
+      sectionRefs.current[list.id] = elem;
     });
 
-    setHeadingTops(headingTops);
-  }, [sections]);
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      let minDiff = Infinity;
+      let closestSection = '';
 
-  useEffect(() => {
-    updateSectionPositions();
-
-    let prevScrollHeight = document.body.scrollHeight;
-    let timeoutId: ReturnType<typeof setTimeout> | null = null;
-
-    const checkScrollHeight = () => {
-      const scrollHeight = document.body.scrollHeight;
-      if (prevScrollHeight !== scrollHeight) {
-        updateSectionPositions();
+      if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 80
+      ) {
+        setActiveSection(sections[sections.length - 1].id);
+        return;
       }
-      prevScrollHeight = scrollHeight;
-      timeoutId = setTimeout(checkScrollHeight, 250);
+
+      sections.forEach((section) => {
+        const elem = sectionRefs.current[section.id];
+
+        if (elem) {
+          const rect = elem.getBoundingClientRect();
+          const sectionTop = rect.top + window.scrollY;
+          const diff = Math.abs(sectionTop - currentScrollY - 160);
+
+          if (diff < minDiff) {
+            minDiff = diff;
+            closestSection = section.id;
+          }
+        }
+      });
+
+      setActiveSection(closestSection);
     };
-    timeoutId = setTimeout(checkScrollHeight, 250);
+
+    window.addEventListener('scroll', handleScroll);
 
     return () => {
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      }
+      window.removeEventListener('scroll', handleScroll);
     };
-  }, [updateSectionPositions]);
-
-  const onScroll = useCallback(() => {
-    const scrollTop = getScrollTop();
-    if (!headingTops) return;
-    const currentHeading = [...headingTops]
-      .reverse()
-      .find((headingTop) => scrollTop >= headingTop.top - 500);
-
-    if (!currentHeading) {
-      setActiveId(null);
-      return;
-    }
-
-    setActiveId(currentHeading.id);
-  }, [headingTops]);
-
-  useEffect(() => {
-    window.addEventListener('scroll', onScroll);
-
-    return () => {
-      window.removeEventListener('scroll', onScroll);
-    };
-  }, [onScroll]);
+  }, []);
 
   return (
     <nav className="sticky top-0 z-20 mb-3.5 flex h-16 w-full items-center justify-between gap-5 whitespace-nowrap bg-white text-lg font-bold sm:gap-0">
@@ -92,7 +62,7 @@ const Nav = ({ sections }: { sections: NavSection[] }) => {
           key={id}
           href={`#${id}`}
           className={`${
-            activeId === id
+            activeSection === id
               ? 'text-sub-color1 underline underline-offset-8'
               : 'text-gray-500'
           }`}
@@ -105,11 +75,3 @@ const Nav = ({ sections }: { sections: NavSection[] }) => {
 };
 
 export default Nav;
-
-export const getScrollTop = () => {
-  if (!document.body) return 0;
-  const scrollTop = document.documentElement
-    ? document.documentElement.scrollTop || document.body.scrollTop
-    : document.body.scrollTop;
-  return scrollTop;
-};
