9a729c2ab93f6f3d97aa82553b1c9db2bce5cd82
hyeonjun-L <beeminor60@gmail.com>
2024-03-13 09:46:47 +0900

    feat(#316): applyBtn disabled props 추가

--- src/components/Button/ApplyButton.tsx
+++ src/components/Button/ApplyButton.tsx
@@ -1,13 +1,23 @@
 interface ApplyButtonProps {
   label: React.ReactNode;
   onClick: () => void;
+  disabled?: boolean;
 }
 
-const ApplyButton = ({ label, onClick }: ApplyButtonProps) => {
+const ApplyButton = ({
+  label,
+  onClick,
+  disabled = false,
+}: ApplyButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="group flex h-[45px] w-full items-center justify-center rounded-md bg-main-color text-xl font-bold text-white hover:bg-[#D34889] active:scale-105"
+      disabled={disabled}
+      className={`group flex h-[45px] w-full items-center justify-center rounded-md text-xl font-bold text-white  active:scale-105 ${
+        disabled
+          ? 'bg-main-color-transparent'
+          : 'bg-main-color hover:bg-[#D34889]'
+      }`}
     >
       {label}
     </button>
