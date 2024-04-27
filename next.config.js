9fc915e47d46192480812dec1d6e50dc06a743a0
hyeonjun-L <beeminor60@gmail.com>
2024-04-28 04:45:07 +0900

    fix/feat(#367): next.js config 여러번 재정의 되는 문제 체인 형식으로 변경 후 해결/ PWA 플러그인 적용

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import("next").NextConfig} */
-
+const { withSentryConfig } = require('@sentry/nextjs');
+const withPWA = require('next-pwa')({
+  disable: process.env.NODE_ENV === 'development',
+  dest: 'public',
+});
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
+const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.module.rules.push({
@@ -29,70 +33,21 @@ module.exports = withBundleAnalyzer({
       },
     ];
   },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/register',
-  //       destination: '/',
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: '/welcome/:nickname',
-  //       destination: '/',
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: '/upload-profile',
-  //       destination: '/',
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: '/error',
-  //       destination: '/',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
-});
-
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require('@sentry/nextjs');
+};
 
 module.exports = withSentryConfig(
-  module.exports,
+  withBundleAnalyzer(withPWA(nextConfig)),
   {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
     silent: true,
     org: 'connection',
     project: 'javascript-nextjs',
   },
   {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
     transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
     tunnelRoute: '/monitoring',
-
-    // Hides source maps from generated client bundles
     hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
     automaticVercelMonitors: true,
   },
 );
